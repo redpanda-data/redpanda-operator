4803312bd0239c524fa555082120a592eaac8abd
Chris Seto <chriskseto@gmail.com>
2024-04-10 11:21:10 -0400

    *: begin migration to nix for environment management
    This commit begins the work to migrate towards using `nix` for managing
    both our CI and development environments to match how the helm-charts
    repository functions.
    
    The changes are unfortunately large due to the existing tangle of into
    and out of docker environments. Here are the concrete steps performed in
    this commit:
    
    - The usage of docker as a CI environment has been hoisted out of
      Taskfile. There is now a single entrypoint for CI,
      `ci/scripts/run-in-nix-docker.sh`, that bootstraps a nix installation
      to then use `flake.nix` for acquiring dependencies.
    - The usage of docker as a build environment has been replaced with the
      usage of `goreleaser`. The final docker containers will now simply
      copy in the resultant cross compiled binaries.
    - `taskfiles/docker.yaml` has been removed for the sake of simplicity.
    - `dev:install-go` has been removed in favor of using nix. This was the
      only dependency moved in this commit as it was actively causing issues
      when testing locally.
    - `GOLANG_TOOLCHAIN_IMAGE` has been removed entirely. This comes at the
      cost of having a fairly heavy pull from nix on each build though it
      doesn't seem that much heavier than what was already in place. If need
      be, cachix or some clever docker caching can be added in later on to
      mitigate any changes.

---
+++ ci/scripts/run-in-nix-docker.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Set -x (echo commands) so users can tell what's happening. Explicitly avoid
+# -e because we're testing a lot of commands that will likely fail.
+set -x
+
+# When running docker/podman through a VM (Podman Desktop, colima, lima,
+# DOCKER_HOST is set, etc), we need the path of the docker socket within that
+# VM in order to provide docker API access within the container. It's a bit
+# difficult to get that path 100% of the time but this should work in most
+# cases:
+
+# For Podman Desktop
+PODMAN_MACHINE_SOCKET=$(podman machine ssh -- podman info -f '{{ .Host.RemoteSocket.Path }}')
+
+# For colima
+COLIMA_SOCKET=$(colima ssh -- docker context inspect -f '{{ .Endpoints.docker.Host }}')
+
+# For docker running on the host (CI). We always want to fallback to this value
+# LAST as it will always return something but will likely be wrong in the case
+# of using a VM.
+DOCKER_HOST_SOCKET=$(docker context inspect -f '{{ .Endpoints.docker.Host }}')
+
+# Perform a bash version of "coalesce".
+DOCKER_SOCKET=${PODMAN_MACHINE_SOCKET:-${COLIMA_SOCKET:-${DOCKER_HOST_SOCKET}}}
+
+if [[ -z $DOCKER_SOCKET ]]; then
+	echo 'Failed to detect docker socket.'
+	exit 1
+fi
+
+# Spray and pray work is done, now we want to fail on errors.
+set -e
+
+# Build the base image and grab the SHA.
+IMAGE_SHA=$(docker build --quiet -f ./ci/docker/nix.Dockerfile .)
+
+docker run --rm -it \
+	-e DOCKER_HOST=unix:///var/run/docker.sock \
+	-e PWD \
+	-e CI \
+	-e COMMIT \
+	-e BRANCH_NAME \
+	-e TAG_NAME \
+	-e PULL_REQUEST \
+	-e PULL_REQUEST_BASE_BRANCH \
+	-e DOCKERHUB_USER \
+	-e DOCKERHUB_TOKEN \
+	-e CLOUDSMITH_USERNAME \
+	-e CLOUDSMITH_API_KEY \
+	-e GITHUB_API_TOKEN \
+	-e RPK_TEST_CLIENT_ID \
+	-e RPK_TEST_CLIENT_SECRET \
+	--privileged \
+	--net=host \
+	--ipc=host \
+	--volume ${DOCKER_SOCKET:7}:/var/run/docker.sock \
+	--volume $(pwd):/work \
+	$IMAGE_SHA "$@"
