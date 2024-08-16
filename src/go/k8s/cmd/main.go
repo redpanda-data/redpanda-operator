16550ec010ccff8fd5d33dfc78bc273b40f708ba
Andrew Stucki <andrew.stucki@redpanda.com>
2024-08-16 14:56:46 -0400

    Remove conversion webhook and drop patch

--- src/go/k8s/cmd/main.go
+++ src/go/k8s/cmd/main.go
@@ -527,12 +527,6 @@ func main() {
 				setupLog.Error(err, "Unable to create webhook", "webhook", "RedpandaConversion")
 				os.Exit(1)
 			}
-
-			setupLog.Info("Setup Topic conversion webhook")
-			if err = (&redpandav1alpha2.Topic{}).SetupWebhookWithManager(mgr); err != nil {
-				setupLog.Error(err, "Unable to create webhook", "webhook", "TopicConversion")
-				os.Exit(1)
-			}
 		}
 
 	case ClusterControllerMode:
