af729741c77c897cd7cc718bc943e3bdbe614e72
Andrew Stucki <andrew.stucki@redpanda.com>
2024-09-16 15:09:01 -0400

    Better test failure handling for acceptance test framework
    This PR does a couple of things:
    
    1. It does a bunch of panic handling due to the way that godog panics
    when FailNow or SkipNow are called so that we can properly use things
    like testify's require or t.Fatal/t.Fail
    2. It extends the way we can register step functions to include a couple
    of new function prototypes where we can make steps *much* more similar
    to a typical go test and not have to either return nil errors everywhere or
    pull something that looks like a testing.T out of a context.

--- harpoon/steps.go
+++ harpoon/steps.go
@@ -9,7 +9,13 @@
 
 package framework
 
-import "github.com/cucumber/godog"
+import (
+	"context"
+	"reflect"
+
+	"github.com/cucumber/godog"
+	internaltesting "github.com/redpanda-data/redpanda-operator/harpoon/internal/testing"
+)
 
 type stepDefinition struct {
 	expression string
@@ -24,6 +30,56 @@ func RegisterStep(expression string, step interface{}) {
 
 func getSteps(ctx *godog.ScenarioContext) {
 	for _, step := range registeredSteps {
-		ctx.Step(step.expression, step.step)
+		ctx.Step(step.expression, injectTestingT(internaltesting.WrapWithPanicHandler(false, internaltesting.ExitBehaviorNone, step.step)))
 	}
+}
+
+type empty struct{}
+
+func injectTestingT(fn interface{}) interface{} {
+	fnValue := reflect.ValueOf(fn)
+	fnType := fnValue.Type()
+	inTypes := []reflect.Type{}
+	for i := 0; i < fnType.NumIn(); i++ {
+		inTypes = append(inTypes, fnType.In(i))
+	}
+	outTypes := []reflect.Type{}
+	for i := 0; i < fnType.NumOut(); i++ {
+		outTypes = append(outTypes, fnType.Out(i))
+	}
+
+	path := reflect.TypeOf(empty{}).PkgPath()
+
+	// handle the case of ctx.Context, TestingT...
+	if len(inTypes) > 1 && isType("context", "Context", inTypes[0]) && isType(path, "TestingT", inTypes[1]) {
+		inTypes = append([]reflect.Type{inTypes[0]}, inTypes[2:]...)
+		fnType := reflect.FuncOf(inTypes, outTypes, false)
+		newFn := reflect.MakeFunc(fnType, func(args []reflect.Value) (results []reflect.Value) {
+			ctx := args[0].Interface().(context.Context)
+			t := T(ctx)
+			newArgs := append([]reflect.Value{args[0], reflect.ValueOf(t)}, args[1:]...)
+			return fnValue.Call(newArgs)
+		}).Interface()
+		return newFn
+	}
+
+	// handle the case of TestingT
+	if len(inTypes) > 0 && isType(path, "TestingT", inTypes[0]) {
+		// inject a ctx.Context, pull TestingT out, and then drop the context
+		inTypes = append([]reflect.Type{reflect.TypeFor[context.Context]()}, inTypes[1:]...)
+		fnType := reflect.FuncOf(inTypes, outTypes, false)
+		newFn := reflect.MakeFunc(fnType, func(args []reflect.Value) (results []reflect.Value) {
+			ctx := args[0].Interface().(context.Context)
+			t := T(ctx)
+			newArgs := append([]reflect.Value{reflect.ValueOf(t)}, args[1:]...)
+			return fnValue.Call(newArgs)
+		}).Interface()
+		return newFn
+	}
+
+	return fn
+}
+
+func isType(pkg, name string, typ reflect.Type) bool {
+	return typ.PkgPath() == pkg && name == typ.Name()
 }
