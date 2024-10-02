c6eb10af03bb5b7f0f8434594bd035d6086c39ad
Andrew Stucki <andrew.stucki@redpanda.com>
2024-10-02 15:48:48 -0400

    Fix up package level docstrings

--- operator/internal/controller/redpanda/redpanda_controller.go
+++ operator/internal/controller/redpanda/redpanda_controller.go
@@ -7,6 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 
+// Package redpanda contains reconciliation logic for cluster.redpanda.com CRDs
 package redpanda
 
 import (
--- operator/internal/controller/vectorized/cluster_controller.go
+++ operator/internal/controller/vectorized/cluster_controller.go
@@ -7,7 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 
-// Package redpanda contains reconciliation logic for redpanda.vectorized.io CRD
+// Package vectorized contains reconciliation logic for redpanda.vectorized.io CRDs
 package vectorized
 
 import (
