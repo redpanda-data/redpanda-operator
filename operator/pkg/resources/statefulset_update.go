1542f9b6134750f8f857d7ecafe26562d94a3410
Johannes Brüderl <johannes@redpanda.com>
2024-11-08 09:00:00 +0100

    operator v1: rename SumNodePoolReplicas to GetDesiredReplicas
    The function is about desired replicas. Rename it to make this clear.

--- operator/pkg/resources/statefulset_update.go
+++ operator/pkg/resources/statefulset_update.go
@@ -573,7 +573,7 @@ func (r *StatefulSetResource) putInMaintenanceMode(ctx context.Context, pod stri
 }
 
 func (r *StatefulSetResource) checkMaintenanceMode(ctx context.Context, pod string) error {
-	if r.pandaCluster.SumNodePoolReplicas() <= 1 {
+	if r.pandaCluster.GetDesiredReplicas() <= 1 {
 		return nil
 	}
 
