fb994706416d76ddbe967979ffc843ffcc543d9f
Rafal Korepta <rafal.korepta@gmail.com>
2024-12-02 18:52:32 +0100

    Run licenseupdater on all go files

--- pkg/gotohelm/internal/bootstrap/bootstrap.go
+++ pkg/gotohelm/internal/bootstrap/bootstrap.go
@@ -1,3 +1,12 @@
+// Copyright 2024 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
 // Welcome to the magical bootstrap package. This package/file generates the
 // _shims.tpl file included in all gotohelm outputs. A task in Taskfile.yaml is
 // used to copy the generated file into the gotohelm package. In the future, it
