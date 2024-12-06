22723a3c4868c81a3016fed8e7308f04ded3286d
Rafal Korepta <rafal.korepta@gmail.com>
2024-12-06 09:09:34 +0100

    Create pipeline for nightly build release

--- ci/scripts/tag-check.sh
+++ ci/scripts/tag-check.sh
@@ -4,6 +4,6 @@ set -eu
 
 if [[ -n "${BUILDKITE_TAG-}" ]]; then
   echo "$BUILDKITE_TAG"
-elif [[ "${NIGHTLY_K8S:-}" == "1" ]]; then
+elif [[ "${NIGHTLY_K8S:-}" == "1" ]] || [[ "${NIGHTLY_RELEASE:-}" == "true" ]]; then
   echo "v0.0.0-$(date --utc +%Y%m%d)git$(git rev-parse --short=7 HEAD)"
 fi
