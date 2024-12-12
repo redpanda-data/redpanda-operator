e24f1c0523fae50de1eb0723af77443db7e4897e
Rafal Korepta <rafal.korepta@gmail.com>
2024-12-12 14:24:21 +0100

    Create CI for charts package

--- ci/overlay.nix
+++ ci/overlay.nix
@@ -5,4 +5,6 @@
   docker-tag-list = pkgs.callPackage ./docker-tag-list.nix { };
   kuttl = pkgs.callPackage ./kuttl.nix { };
   setup-envtest = pkgs.callPackage ./setup-envtest.nix { };
+  helm-3-10-3 = pkgs.callPackage ./helm.nix { };
+  chart-testing = pkgs.callPackage ./chart-testing.nix { };
 })
