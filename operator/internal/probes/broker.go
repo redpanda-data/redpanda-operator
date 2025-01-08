bdcdc72c007abad02efc2d2f807936da7f354db3
Andrew Stucki <andrew.stucki@redpanda.com>
2025-01-08 14:54:22 -0500

    Initial health probe work

---
+++ operator/internal/probes/broker.go
@@ -0,0 +1,189 @@
+package probes
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	"github.com/redpanda-data/common-go/rpadmin"
+	rpkconfig "github.com/redpanda-data/redpanda/src/go/rpk/pkg/config"
+	"github.com/spf13/afero"
+	"k8s.io/utils/ptr"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	internalclient "github.com/redpanda-data/redpanda-operator/operator/pkg/client"
+)
+
+type Option func(*Prober)
+
+func WithFactory(factory internalclient.ClientFactory) Option {
+	return func(prober *Prober) {
+		prober.factory = factory
+	}
+}
+
+func WithLogger(logger logr.Logger) Option {
+	return func(prober *Prober) {
+		prober.logger = logger
+	}
+}
+
+func WithFS(fs afero.Fs) Option {
+	return func(prober *Prober) {
+		prober.fs = fs
+	}
+}
+
+// Prober wraps the logic used in checking broker health and readiness.
+type Prober struct {
+	logger     logr.Logger
+	factory    internalclient.ClientFactory
+	configPath string
+	fs         afero.Fs
+}
+
+func NewProber(mgr ctrl.Manager, configPath string, options ...Option) *Prober {
+	prober := &Prober{
+		configPath: configPath,
+		fs:         afero.NewOsFs(),
+		logger:     mgr.GetLogger(),
+		factory:    internalclient.NewFactory(mgr.GetConfig(), mgr.GetClient()),
+	}
+
+	for _, opt := range options {
+		opt(prober)
+	}
+
+	return prober
+}
+
+// IsClusterBrokerHealthy checks that a cluster broker is up and ready to serve requests
+// but additionally serves as a gate for StatefulSet rolling restarts. It's a relaxed form
+// of our previous readiness probe which only looked at the `IsHealthy` return value from
+// the cluster health overview endpoint. This had the unfortunate effect of marking all brokers
+// as "unready" when a single broker was marked as a downed node due to being a reflection of
+// the overall cluster health rather than an individual broker's health.
+func (p *Prober) IsClusterBrokerHealthy(ctx context.Context, brokerID int) (bool, error) {
+	client, err := p.getClient(ctx, brokerID)
+	if err != nil {
+		return false, fmt.Errorf("initializing client to check broker health: %w", err)
+	}
+	defer client.Close()
+
+	// This check is a more relaxed version of our previous "readiness" probe that was
+	// based solely off of the cluster health. It works via doing the following:
+	//
+	// 1. Gets the broker status, making sure it's marked as `is_alive` and is not in maintenance mode.
+	// 2. Lists the local summary of partitions for the broker, making sure there are no leaderless or
+	//    underreplicated partitions.
+	// 3. Gets the cluster health and makes sure that the broker is part of the quorum by having a
+	//    controller id set.
+	//
+	// These allow us to ensure we aren't fully coupled to the overall cluster health status,
+	// which previously caused brokers to be marked as "not ready" when *any* broker in the cluster
+	// was unhealthy.
+
+	broker, err := client.Broker(ctx, brokerID)
+	if err != nil {
+		return false, fmt.Errorf("fetching broker status: %w", err)
+	}
+
+	// is the broker proxied API down?
+	if !ptr.Deref(broker.IsAlive, false) {
+		return false, nil
+	}
+
+	// is the broker in maintenance mode?
+	if broker.Maintenance != nil {
+		return false, nil
+	}
+
+	summary, err := client.GetLocalPartitionsSummary(ctx)
+	if err != nil {
+		return false, fmt.Errorf("fetching broker partitions: %w", err)
+	}
+
+	// do we have any leaderless or under-replicated nodes?
+	if summary.Leaderless != 0 || summary.UnderReplicated != 0 {
+		return false, nil
+	}
+
+	clusterHealth, err := client.GetHealthOverview(ctx)
+	if err != nil {
+		return false, fmt.Errorf("fetching cluster health: %w", err)
+	}
+
+	// do we have an elected cluster leader (i.e. are we part of quorum)
+	if clusterHealth.ControllerID < 0 {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+// IsClusterBrokerReady checks if a cluster broker should be servicing requests. It does
+// this by issuing a request to the given broker which should get proxied through to the
+// underlying Kafka API, checking that the broker is marked as alive, not in maintenance
+// mode, and that we actually get a response should inform us that:
+//
+// 1. The Kafka API is up and servicing requests.
+// 2. The Admin API is up and servicing requests.
+// 3. The broker is not in a mode where it shouldn't be servicing anything (i.e. maintenance).
+func (p *Prober) IsClusterBrokerReady(ctx context.Context, brokerID int) (bool, error) {
+	client, err := p.getClient(ctx, brokerID)
+	if err != nil {
+		return false, fmt.Errorf("initializing client to check broker readiness: %w", err)
+	}
+	defer client.Close()
+
+	// unlike the above check which includes cluster quorum and partition status, this just
+	// checks that a broker is up and not in maintenance mode
+
+	broker, err := client.Broker(ctx, brokerID)
+	if err != nil {
+		return false, fmt.Errorf("fetching broker status: %w", err)
+	}
+
+	// is the broker proxied API down?
+	if !ptr.Deref(broker.IsAlive, false) {
+		return false, nil
+	}
+
+	// is the broker in maintenance mode?
+	if broker.Maintenance != nil {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (p *Prober) getClient(ctx context.Context, brokerID int) (*rpadmin.AdminAPI, error) {
+	profile, err := p.readProfile()
+	if err != nil {
+		return nil, fmt.Errorf("reading profile: %w", err)
+	}
+
+	client, err := p.factory.RedpandaAdminClient(ctx, profile)
+	if err != nil {
+		return nil, fmt.Errorf("initializing client: %w", err)
+	}
+	defer client.Close()
+
+	scopedClient, err := client.ForBroker(ctx, brokerID)
+	if err != nil {
+		return nil, fmt.Errorf("initializing client for broker %d: %w", brokerID, err)
+	}
+
+	return scopedClient, nil
+}
+
+func (p *Prober) readProfile() (*rpkconfig.RpkProfile, error) {
+	params := rpkconfig.Params{ConfigFlag: p.configPath}
+
+	config, err := params.Load(p.fs)
+	if err != nil {
+		return nil, fmt.Errorf("loading profile: %w", err)
+	}
+
+	return config.VirtualProfile(), nil
+}
