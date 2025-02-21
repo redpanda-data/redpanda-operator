87461251958a5a7e71ca7c7020d1152733b8f646
Paul Zhang <paul@redpanda.com>
2025-02-21 10:58:33 -0600

    Support multiple external listeners in Operator v1 vectorized Cluster.
    (cherry picked from commit 83d4748f65a1e7f6a1d5282f726604d917b0f0d2)

--- operator/cmd/configurator/configurator_test.go
+++ operator/cmd/configurator/configurator_test.go
@@ -38,6 +38,7 @@ func TestPopulateRack(t *testing.T) {
 
 func TestAdditionalListeners(t *testing.T) { //nolint
 	sasl := "sasl"
+	mtls := "mtls_identity"
 	httpBasic := "http_basic"
 	tests := []struct {
 		name                            string
@@ -51,6 +52,8 @@ func TestAdditionalListeners(t *testing.T) { //nolint
 		expectedPandaProxyAPI           []config.NamedAuthNSocketAddress
 		expectedadvertisedPandaProxyAPI []config.NamedSocketAddress
 		expectedPandaProxyTLS           []config.ServerTLS
+		expectedSchemaRegistryAPI       []config.NamedAuthNSocketAddress
+		expectedSchemaRegistryAPITLS    []config.ServerTLS
 		expectedError                   bool
 	}{
 		{
@@ -225,7 +228,7 @@ func TestAdditionalListeners(t *testing.T) { //nolint
 		},
 		{
 			name: "additional kafka and proxy listeners with mTLS",
-			addtionalListenersCfg: `{"redpanda.advertised_kafka_api":"[{'name': 'private-link-kafka', 'address': '{{ .Index }}-f415bda0-{{ .HostIP | sha256sum | substr 0 7 }}.redpanda.com', 'port': {{39002 | add .Index}}}]",` +
+			addtionalListenersCfg: `{"redpanda.advertised_kafka_api":"[{\"name\": \"private-link-kafka\", 'address': '{{ .Index }}-f415bda0-{{ .HostIP | sha256sum | substr 0 7 }}.redpanda.com', 'port': {{39002 | add .Index}}}]",` +
 				`"redpanda.kafka_api":"[{'name': 'private-link-kafka', 'address': '0.0.0.0', 'port': {{39002 | add .Index}}, 'authentication_method': 'sasl'}]",` +
 				`"pandaproxy.advertised_pandaproxy_api":"[{'name': 'private-link-proxy', 'address': '{{ .Index }}-f415bda0-{{ .HostIP | sha256sum | substr 0 7 }}.redpanda.com', 'port': {{30282 | add .Index}}}]",` +
 				`"pandaproxy.pandaproxy_api":"[{'name': 'private-link-proxy', 'address': '0.0.0.0', 'port': {{30282 | add .Index}}, 'authentication_method': 'sasl'}]"}`,
@@ -346,6 +349,253 @@ func TestAdditionalListeners(t *testing.T) { //nolint
 				},
 			},
 		},
+		{
+			name: "additional schema registry listeners with mTLS",
+			addtionalListenersCfg: `{"schema_registry.schema_registry_api":"[{'name': 'schema-registry-mtls', 'address': '0.0.0.0', 'port': 30083, 'authentication_method':'http_basic'},{'name':'schema-registry','address':'should be ingored because of dup to the one in nodeCfg'}]",` +
+				`"schema_registry.schema_registry_api_tls":"[{'name': 'schema-registry-mtls', 'truststore_file':'/etc/tls/certs/schema-registry/ca.crt'}]"}`,
+			hostIndex: 1,
+			hostIP:    "192.168.0.1",
+			nodeCfg: config.RedpandaYaml{
+				SchemaRegistry: &config.SchemaRegistry{
+					SchemaRegistryAPI: []config.NamedAuthNSocketAddress{{
+						Name:    "schema-registry",
+						Address: "0.0.0.0",
+						Port:    30081,
+						AuthN:   &httpBasic,
+					}},
+					SchemaRegistryAPITLS: []config.ServerTLS{{
+						Name:     "schema-registry",
+						CertFile: "/etc/tls/certs/schema-registry/tls.crt",
+						KeyFile:  "/etc/tls/certs/schema-registry/tls.key",
+						Enabled:  true,
+					}},
+				},
+			},
+			expectedSchemaRegistryAPI: []config.NamedAuthNSocketAddress{
+				{
+					Name:    "schema-registry",
+					Address: "0.0.0.0",
+					Port:    30081,
+					AuthN:   &httpBasic,
+				},
+				{
+					Name:    "schema-registry-mtls",
+					Address: "0.0.0.0",
+					Port:    30083,
+					AuthN:   &httpBasic,
+				},
+			},
+			expectedSchemaRegistryAPITLS: []config.ServerTLS{
+				{
+					Name:     "schema-registry",
+					CertFile: "/etc/tls/certs/schema-registry/tls.crt",
+					KeyFile:  "/etc/tls/certs/schema-registry/tls.key",
+					Enabled:  true,
+				},
+				{
+					Name:              "schema-registry-mtls",
+					CertFile:          "/etc/tls/certs/schema-registry/tls.crt",
+					KeyFile:           "/etc/tls/certs/schema-registry/tls.key",
+					RequireClientAuth: true,
+					TruststoreFile:    "/etc/tls/certs/schema-registry/ca.crt",
+					Enabled:           true,
+				},
+			},
+		},
+		{
+			name: "kafka with SASL + mTLS",
+			addtionalListenersCfg: `{"redpanda.advertised_kafka_api":"[{\"name\": \"mtls-kafka\", 'address': '{{ .Index }}-f415bda0-{{ .HostIP | sha256sum | substr 0 7 }}.redpanda.com', 'port': {{39002 | add .Index}}}]",` +
+				`"redpanda.kafka_api":"[{'name': 'mtls-kafka','address':'0.0.0.0','port': {{39002 | add .Index}},'authentication_method':'mtls_identity'},{'name':'kafka-external','address':'ignored due to dup in nodeCfg'}]", ` +
+				`"redpanda.kafka_api_tls":"[{'name': 'mtls-kafka', 'require_client_auth': true, 'truststore_file':'/etc/tls/certs/kafka-api/ca.crt'}]"}`,
+			hostIndex: 1,
+			hostIP:    "192.168.0.1",
+			nodeCfg: config.RedpandaYaml{
+				Redpanda: config.RedpandaNodeConfig{
+					KafkaAPI: []config.NamedAuthNSocketAddress{
+						{
+							Name:    "kafka-internal",
+							Address: "0.0.0.0",
+							Port:    9092,
+							AuthN:   &sasl,
+						},
+						{
+							Name:    resources.ExternalListenerName,
+							Address: "0.0.0.0",
+							Port:    30092,
+							AuthN:   &sasl,
+						},
+					},
+					AdvertisedKafkaAPI: []config.NamedSocketAddress{
+						{
+							Name:    "kafka-internal",
+							Address: "cluster1.redpanda.svc.cluster.local",
+							Port:    9092,
+						},
+						{
+							Name:    resources.ExternalListenerName,
+							Address: "cluster123.redpanda.com",
+							Port:    30092,
+						},
+					},
+					KafkaAPITLS: []config.ServerTLS{{
+						Name:     resources.ExternalListenerName,
+						CertFile: "crt1.pem",
+						KeyFile:  "key1.pem",
+					}},
+				},
+			},
+			expectedKafkaAPI: []config.NamedAuthNSocketAddress{
+				{
+					Name:    "kafka-internal",
+					Address: "0.0.0.0",
+					Port:    9092,
+					AuthN:   &sasl,
+				},
+				{
+					Address: "0.0.0.0",
+					Name:    resources.ExternalListenerName,
+					Port:    30092,
+					AuthN:   &sasl,
+				},
+				{
+					Address: "0.0.0.0",
+					Name:    "mtls-kafka",
+					Port:    39002 + 1,
+					AuthN:   &mtls,
+				},
+			},
+			expectedAdvertisedKafkaAPI: []config.NamedSocketAddress{
+				{
+					Name:    "kafka-internal",
+					Address: "cluster1.redpanda.svc.cluster.local",
+					Port:    9092,
+				},
+				{
+					Name:    resources.ExternalListenerName,
+					Address: "cluster123.redpanda.com",
+					Port:    30092,
+				},
+				{
+					Address: "1-f415bda0-37d7a80.redpanda.com",
+					Name:    "mtls-kafka",
+					Port:    39002 + 1,
+				},
+			},
+			expectedKafkaAPITLS: []config.ServerTLS{
+				{
+					Name:     resources.ExternalListenerName,
+					CertFile: "crt1.pem",
+					KeyFile:  "key1.pem",
+				},
+				{
+					Name:              "mtls-kafka",
+					CertFile:          "crt1.pem",
+					KeyFile:           "key1.pem",
+					RequireClientAuth: true,
+					TruststoreFile:    "/etc/tls/certs/kafka-api/ca.crt",
+					Enabled:           true,
+				},
+			},
+		},
+		{
+			name: "kafka with mTLS + SASL",
+			addtionalListenersCfg: `{"redpanda.advertised_kafka_api":"[{\"name\": \"sasl-kafka\", 'port': {{39002 | add .Index}}},{'name':'mtls-kafka','port':{{39002 | add .Index}},'address':'ignored due to dup in nodeCfg'}]",` +
+				`"redpanda.kafka_api":"[{'name': 'sasl-kafka','address': '0.0.0.0','port': {{39002 | add .Index}},'authentication_method':'sasl'}]",` +
+				`"redpanda.kafka_api_tls":"[{'name': 'sasl-kafka', 'require_client_auth': false},{'name':'mtls-kafka','cert_file':'ignored due to dup in nodeCfg'}]"}`,
+			hostIndex: 1,
+			hostIP:    "192.168.0.1",
+			nodeCfg: config.RedpandaYaml{
+				Redpanda: config.RedpandaNodeConfig{
+					KafkaAPI: []config.NamedAuthNSocketAddress{
+						{
+							Name:    "kafka-internal",
+							Address: "0.0.0.0",
+							Port:    9092,
+							AuthN:   &sasl,
+						},
+						{
+							Name:    "mtls-kafka",
+							Address: "0.0.0.0",
+							Port:    30092,
+							AuthN:   &mtls,
+						},
+					},
+					AdvertisedKafkaAPI: []config.NamedSocketAddress{
+						{
+							Name:    "kafka-internal",
+							Address: "cluster1.redpanda.svc.cluster.local",
+							Port:    9092,
+						},
+						{
+							Name:    "mtls-kafka",
+							Address: "cluster123.redpanda.com",
+							Port:    30092,
+						},
+					},
+					KafkaAPITLS: []config.ServerTLS{{
+						Name:              "mtls-kafka",
+						CertFile:          "crt1.pem",
+						KeyFile:           "key1.pem",
+						TruststoreFile:    "/etc/tls/certs/kafka-api/ca.crt",
+						Enabled:           true,
+						RequireClientAuth: true,
+					}},
+				},
+			},
+			expectedKafkaAPI: []config.NamedAuthNSocketAddress{
+				{
+					Name:    "kafka-internal",
+					Address: "0.0.0.0",
+					Port:    9092,
+					AuthN:   &sasl,
+				},
+				{
+					Address: "0.0.0.0",
+					Name:    "mtls-kafka",
+					Port:    30092,
+					AuthN:   &mtls,
+				},
+				{
+					Address: "0.0.0.0",
+					Name:    "sasl-kafka",
+					Port:    39002 + 1,
+					AuthN:   &sasl,
+				},
+			},
+			expectedAdvertisedKafkaAPI: []config.NamedSocketAddress{
+				{
+					Name:    "kafka-internal",
+					Address: "cluster1.redpanda.svc.cluster.local",
+					Port:    9092,
+				},
+				{
+					Name:    "mtls-kafka",
+					Address: "cluster123.redpanda.com",
+					Port:    30092,
+				},
+				{
+					Address: "cluster123.redpanda.com",
+					Name:    "sasl-kafka",
+					Port:    39002 + 1,
+				},
+			},
+			expectedKafkaAPITLS: []config.ServerTLS{
+				{
+					Name:              "mtls-kafka",
+					CertFile:          "crt1.pem",
+					KeyFile:           "key1.pem",
+					RequireClientAuth: true,
+					TruststoreFile:    "/etc/tls/certs/kafka-api/ca.crt",
+					Enabled:           true,
+				},
+				{
+					Name:     "sasl-kafka",
+					CertFile: "crt1.pem",
+					KeyFile:  "key1.pem",
+					Enabled:  true,
+				},
+			},
+		},
 	}
 	for i := 0; i < len(tests); i++ {
 		tt := &tests[i]
@@ -354,9 +604,15 @@ func TestAdditionalListeners(t *testing.T) { //nolint
 			assert.Error(t, err)
 		} else {
 			assert.NoError(t, err)
-			validateListenerConfig(t, tt.expectedKafkaAPI, tt.nodeCfg.Redpanda.KafkaAPI, func(c config.NamedAuthNSocketAddress) string { return c.Name })
-			validateListenerConfig(t, tt.expectedAdvertisedKafkaAPI, tt.nodeCfg.Redpanda.AdvertisedKafkaAPI, func(c config.NamedSocketAddress) string { return c.Name })
-			validateListenerConfig(t, tt.expectedKafkaAPITLS, tt.nodeCfg.Redpanda.KafkaAPITLS, func(c config.ServerTLS) string { return c.Name })
+			if len(tt.expectedKafkaAPI) > 0 {
+				validateListenerConfig(t, tt.expectedKafkaAPI, tt.nodeCfg.Redpanda.KafkaAPI, func(c config.NamedAuthNSocketAddress) string { return c.Name })
+			}
+			if len(tt.expectedAdvertisedKafkaAPI) > 0 {
+				validateListenerConfig(t, tt.expectedAdvertisedKafkaAPI, tt.nodeCfg.Redpanda.AdvertisedKafkaAPI, func(c config.NamedSocketAddress) string { return c.Name })
+			}
+			if len(tt.expectedKafkaAPITLS) > 0 {
+				validateListenerConfig(t, tt.expectedKafkaAPITLS, tt.nodeCfg.Redpanda.KafkaAPITLS, func(c config.ServerTLS) string { return c.Name })
+			}
 			if len(tt.expectedPandaProxyAPI) > 0 {
 				validateListenerConfig(t, tt.expectedPandaProxyAPI, tt.nodeCfg.Pandaproxy.PandaproxyAPI, func(c config.NamedAuthNSocketAddress) string { return c.Name })
 			}
@@ -365,6 +621,12 @@ func TestAdditionalListeners(t *testing.T) { //nolint
 			}
 			if len(tt.expectedPandaProxyTLS) > 0 {
 				validateListenerConfig(t, tt.expectedPandaProxyTLS, tt.nodeCfg.Pandaproxy.PandaproxyAPITLS, func(c config.ServerTLS) string { return c.Name })
+			}
+			if len(tt.expectedSchemaRegistryAPI) > 0 {
+				validateListenerConfig(t, tt.expectedSchemaRegistryAPI, tt.nodeCfg.SchemaRegistry.SchemaRegistryAPI, func(c config.NamedAuthNSocketAddress) string { return c.Name })
+			}
+			if len(tt.expectedSchemaRegistryAPITLS) > 0 {
+				validateListenerConfig(t, tt.expectedSchemaRegistryAPITLS, tt.nodeCfg.SchemaRegistry.SchemaRegistryAPITLS, func(c config.ServerTLS) string { return c.Name })
 			}
 		}
 	}
