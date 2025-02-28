8a46ad0fe59786f32c42757f4906e3be9497b20f
Johannes Brüderl <johannes@redpanda.com>
2025-02-28 10:26:36 +0100

    operator v1: support new ghost broker decommissioner
    - add flag to enable this new controller (it's previously only
      available as sidecar)
    - update factory to create admin api for v1 clusters
    - add log when decommissioning a broke

--- operator/pkg/client/cluster.go
+++ operator/pkg/client/cluster.go
@@ -10,12 +10,18 @@
 package client
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/redpanda-data/common-go/rpadmin"
 	"github.com/twmb/franz-go/pkg/kgo"
 	"github.com/twmb/franz-go/pkg/sr"
 
 	redpanda "github.com/redpanda-data/redpanda-operator/charts/redpanda/client"
 	redpandav1alpha2 "github.com/redpanda-data/redpanda-operator/operator/api/redpanda/v1alpha2"
+	vectorizedv1alpha1 "github.com/redpanda-data/redpanda-operator/operator/api/vectorized/v1alpha1"
+	"github.com/redpanda-data/redpanda-operator/operator/pkg/admin"
+	"github.com/redpanda-data/redpanda-operator/operator/pkg/resources/certmanager"
 )
 
 // redpandaAdminForCluster returns a simple rpadmin.AdminAPI able to communicate with the given cluster specified via a Redpanda cluster.
@@ -38,6 +44,25 @@ func (c *Factory) redpandaAdminForCluster(cluster *redpandav1alpha2.Redpanda) (*
 	}
 
 	return client, nil
+}
+
+func (c *Factory) redpandaAdminForV1Cluster(cluster *vectorizedv1alpha1.Cluster) (*rpadmin.AdminAPI, error) {
+	ctx := context.Background()
+
+	if cluster.AdminAPITLS() != nil {
+		return nil, fmt.Errorf("non-TLS admin API is not supported on V1 CRD")
+	}
+	// Assume no TLS. Practically, we don't need to support it in Operator V1.
+	t := &certmanager.ClusterCertificates{}
+
+	a, err := admin.NewNodePoolInternalAdminAPI(ctx, c.Client, cluster, fmt.Sprintf("%s.%s.svc.cluster.local", cluster.Name, cluster.Namespace), t, c.dialer)
+	if err != nil {
+		return nil, err
+	}
+	// It's weird that the V1 admin factory returns an interface instead of the
+	// rpadmin struct. We'll cast it back; it's ugly but since this is a legacy
+	// code path we don't care too much.
+	return a.(*rpadmin.AdminAPI), nil
 }
 
 // schemaRegistryForCluster returns a simple sr.Client able to communicate with the given cluster specified via a Redpanda cluster.
