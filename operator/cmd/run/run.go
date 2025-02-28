ec09e1f500bdb19571286bafdc927cbd00276f14
Johannes Brüderl <johannes@redpanda.com>
2025-02-28 10:26:36 +0100

    operator v1: support new ghost broker decommissioner
    - add flag to enable this new controller (it's previously only
      available as sidecar)
    - update factory to create admin api for v1 clusters
    - add log when decommissioning a broke

--- operator/cmd/run/run.go
+++ operator/cmd/run/run.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 	"time"
 
@@ -25,12 +26,16 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	helmkube "helm.sh/helm/v3/pkg/kube"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/certwatcher"
+	kubeClient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
@@ -120,36 +125,37 @@ func (s *LabelSelectorValue) Type() string {
 
 func Command() *cobra.Command {
 	var (
-		clusterDomain               string
-		metricsAddr                 string
-		secureMetrics               bool
-		enableHTTP2                 bool
-		probeAddr                   string
-		pprofAddr                   string
-		enableLeaderElection        bool
-		webhookEnabled              bool
-		configuratorBaseImage       string
-		configuratorTag             string
-		configuratorImagePullPolicy string
-		decommissionWaitInterval    time.Duration
-		metricsTimeout              time.Duration
-		restrictToRedpandaVersion   string
-		namespace                   string
-		additionalControllers       []string
-		operatorMode                bool
-		enableHelmControllers       bool
-		ghostbuster                 bool
-		unbindPVCsAfter             time.Duration
-		unbinderSelector            LabelSelectorValue
-		autoDeletePVCs              bool
-		forceDefluxedMode           bool
-		helmRepositoryURL           string
-		webhookCertPath             string
-		webhookCertName             string
-		webhookCertKey              string
-		metricsCertPath             string
-		metricsCertName             string
-		metricsCertKey              string
+		clusterDomain                   string
+		metricsAddr                     string
+		secureMetrics                   bool
+		enableHTTP2                     bool
+		probeAddr                       string
+		pprofAddr                       string
+		enableLeaderElection            bool
+		webhookEnabled                  bool
+		configuratorBaseImage           string
+		configuratorTag                 string
+		configuratorImagePullPolicy     string
+		decommissionWaitInterval        time.Duration
+		metricsTimeout                  time.Duration
+		restrictToRedpandaVersion       string
+		namespace                       string
+		additionalControllers           []string
+		operatorMode                    bool
+		enableHelmControllers           bool
+		ghostbuster                     bool
+		unbindPVCsAfter                 time.Duration
+		unbinderSelector                LabelSelectorValue
+		autoDeletePVCs                  bool
+		forceDefluxedMode               bool
+		helmRepositoryURL               string
+		webhookCertPath                 string
+		webhookCertName                 string
+		webhookCertKey                  string
+		metricsCertPath                 string
+		metricsCertName                 string
+		metricsCertKey                  string
+		enableGhostBrokerDecommissioner bool
 	)
 
 	cmd := &cobra.Command{
@@ -190,6 +196,7 @@ func Command() *cobra.Command {
 				metricsCertPath,
 				metricsCertName,
 				metricsCertKey,
+				enableGhostBrokerDecommissioner,
 			)
 		},
 	}
@@ -234,6 +241,7 @@ func Command() *cobra.Command {
 		"The directory that contains the metrics server certificate.")
 	cmd.Flags().StringVar(&metricsCertName, "metrics-cert-name", "tls.crt", "The name of the metrics server certificate file.")
 	cmd.Flags().StringVar(&metricsCertKey, "metrics-cert-key", "tls.key", "The name of the metrics server key file.")
+	cmd.Flags().BoolVar(&enableGhostBrokerDecommissioner, "enable-ghost-broker-decommissioner", false, "Enable ghost broker decommissioner.")
 
 	// 3rd party flags.
 	clientOptions.BindFlags(cmd.Flags())
@@ -244,6 +252,21 @@ func Command() *cobra.Command {
 	cmd.Flags().String("events-addr", "", "A deprecated and unused flag")
 
 	return cmd
+}
+
+type v1Fetcher struct {
+	client kubeClient.Client
+}
+
+func (f *v1Fetcher) FetchLatest(ctx context.Context, name, namespace string) (any, error) {
+	var vectorizedCluster vectorizedv1alpha1.Cluster
+	if err := f.client.Get(ctx, types.NamespacedName{
+		Name:      name,
+		Namespace: namespace,
+	}, &vectorizedCluster); err != nil {
+		return nil, err
+	}
+	return &vectorizedCluster, nil
 }
 
 //nolint:funlen,gocyclo // length looks good
@@ -279,6 +302,7 @@ func Run(
 	metricsCertPath string,
 	metricsCertName string,
 	metricsCertKey string,
+	enableGhostBrokerDecommissioner bool,
 ) error {
 	setupLog := ctrl.LoggerFrom(ctx).WithName("setup")
 
@@ -631,6 +655,43 @@ func Run(
 			Selector: unbinderSelector,
 		}).SetupWithManager(mgr); err != nil {
 			setupLog.Error(err, "unable to create controller", "controller", "PVCUnbinder")
+			return err
+		}
+	}
+
+	if enableGhostBrokerDecommissioner {
+		d := decommissioning.NewStatefulSetDecommissioner(mgr, &v1Fetcher{client: mgr.GetClient()},
+			decommissioning.WithCleanupPVCs(false),
+			decommissioning.WithFactory(internalclient.NewFactory(mgr.GetConfig(), mgr.GetClient())),
+			decommissioning.WithFilter(func(ctx context.Context, sts *appsv1.StatefulSet) (bool, error) {
+				idx := slices.IndexFunc(
+					sts.OwnerReferences,
+					func(ownerRef metav1.OwnerReference) bool {
+						return ownerRef.APIVersion == vectorizedv1alpha1.GroupVersion.String() && ownerRef.Kind == "Cluster"
+					})
+				if idx == -1 {
+					return false, nil
+				}
+
+				var vectorizedCluster vectorizedv1alpha1.Cluster
+				if err := mgr.GetClient().Get(ctx, types.NamespacedName{
+					Name:      sts.OwnerReferences[idx].Name,
+					Namespace: sts.Namespace,
+				}, &vectorizedCluster); err != nil {
+					return false, fmt.Errorf("could not get Cluster: %w", err)
+				}
+
+				managedAnnotationKey := vectorizedv1alpha1.GroupVersion.Group + "/managed"
+				if managed, exists := vectorizedCluster.Annotations[managedAnnotationKey]; exists && managed == "false" {
+					ctrl.Log.V(1).Info("ignoring StatefulSet of unmanaged V1 Cluster", "sts", sts.Name, "namespace", sts.Namespace)
+					return false, nil
+				}
+
+				return true, nil
+			}),
+		)
+		if err := d.SetupWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create controller", "controller", "StatefulSetDecommissioner")
 			return err
 		}
 	}
--- operator/pkg/client/factory_test.go
+++ operator/pkg/client/factory_test.go
@@ -20,18 +20,29 @@ import (
 	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
 	cmmetav1 "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
 	"github.com/go-logr/logr"
+	"github.com/go-logr/logr/testr"
+	"github.com/redpanda-data/common-go/rpadmin"
 	"github.com/stretchr/testify/require"
 	"github.com/twmb/franz-go/pkg/kadm"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/ptr"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	redpandav1alpha2 "github.com/redpanda-data/redpanda-operator/operator/api/redpanda/v1alpha2"
+	vectorizedv1alpha1 "github.com/redpanda-data/redpanda-operator/operator/api/vectorized/v1alpha1"
+	crds "github.com/redpanda-data/redpanda-operator/operator/config/crd/bases"
 	"github.com/redpanda-data/redpanda-operator/operator/internal/controller"
+	"github.com/redpanda-data/redpanda-operator/operator/internal/controller/vectorized"
+	"github.com/redpanda-data/redpanda-operator/operator/internal/testenv"
+	"github.com/redpanda-data/redpanda-operator/operator/pkg/admin"
 	"github.com/redpanda-data/redpanda-operator/operator/pkg/k3d"
+	"github.com/redpanda-data/redpanda-operator/operator/pkg/resources"
 	"github.com/redpanda-data/redpanda-operator/pkg/helm"
 	"github.com/redpanda-data/redpanda-operator/pkg/kube"
 	"github.com/redpanda-data/redpanda-operator/pkg/testutil"
@@ -81,6 +92,118 @@ func (f *fakeObject) GetKafkaAPISpec() *redpandav1alpha2.KafkaAPISpec {
 
 func (f *fakeObject) DeepCopyObject() runtime.Object {
 	return f
+}
+
+func TestIntegrationFactoryOperatorV1(t *testing.T) {
+	testutil.SkipIfNotIntegration(t)
+
+	scheme := controller.V1Scheme
+
+	env := testenv.New(t, testenv.Options{
+		Scheme: scheme,
+		CRDs:   crds.All(),
+		Logger: testr.New(t),
+	})
+
+	var clientFactory *Factory
+	var r *vectorized.ClusterReconciler
+
+	c := env.Client()
+	err := c.Create(context.Background(), &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: env.Namespace(),
+		},
+	})
+	require.NoError(t, err)
+
+	err = c.Create(context.Background(), &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: env.Namespace(),
+		},
+		Subjects: []rbacv1.Subject{
+			{Kind: "ServiceAccount", Namespace: env.Namespace(), Name: "test"},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     "cluster-admin",
+		},
+	})
+	require.NoError(t, err)
+
+	env.SetupManager("test", func(mgr ctrl.Manager) error {
+		dialer := kube.NewPodDialer(mgr.GetConfig())
+		clientFactory = NewFactory(mgr.GetConfig(), mgr.GetClient()).WithDialer(dialer.DialContext)
+
+		r = &vectorized.ClusterReconciler{
+			Client:                mgr.GetClient(),
+			Log:                   testr.New(t),
+			AdminAPIClientFactory: admin.NewNodePoolInternalAdminAPI,
+			Dialer:                dialer.DialContext,
+			Scheme:                scheme,
+		}
+
+		// TODO: this is not optimal, we're using hardcoded docker image name, tag
+		// only until we have better tooling to use a local image
+		r.WithConfiguratorSettings(resources.ConfiguratorSettings{
+			ImagePullPolicy:       corev1.PullIfNotPresent,
+			ConfiguratorBaseImage: "docker.io/redpandadata/redpanda-operator-nightly",
+			ConfiguratorTag:       "v0.0.0-20250129gita89e202",
+		})
+		r.WithClusterDomain("cluster.local")
+
+		return r.SetupWithManager(mgr)
+	})
+
+	cr := vectorizedv1alpha1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test",
+		},
+		Spec: vectorizedv1alpha1.ClusterSpec{
+			Image:    "docker.io/redpandadata/redpanda",
+			Version:  "v24.3.5",
+			Replicas: ptr.To(int32(1)),
+			Configuration: vectorizedv1alpha1.RedpandaConfig{
+				RPCServer: vectorizedv1alpha1.SocketAddress{
+					Port: 33145,
+				},
+				KafkaAPI: []vectorizedv1alpha1.KafkaAPI{
+					{
+						Port: 9092,
+					},
+				},
+				AdminAPI: []vectorizedv1alpha1.AdminAPI{
+					{
+						Port: 9644,
+					},
+				},
+				DeveloperMode: true,
+			},
+		},
+	}
+
+	err = env.Client().Create(testutil.Context(t), &cr)
+	require.NoError(t, err)
+
+	require.Eventuallyf(t, func() bool {
+		cluster := vectorizedv1alpha1.Cluster{}
+		if err := env.Client().Get(testutil.Context(t), types.NamespacedName{
+			Name: "test",
+		}, &cluster); err != nil {
+			return false
+		}
+		return cluster.Status.GetConditionStatus(vectorizedv1alpha1.OperatorQuiescentConditionType) == corev1.ConditionTrue
+	}, time.Minute*5, time.Second*5, "didn't work")
+
+	adminClient, err := clientFactory.RedpandaAdminClient(testutil.Context(t), &cr)
+	require.NoError(t, err)
+
+	brokers, err := adminClient.Brokers(context.Background())
+	require.NoError(t, err)
+	require.Len(t, brokers, 1)
+	require.Equal(t, rpadmin.MembershipStatusActive, brokers[0].MembershipStatus)
 }
 
 func TestIntegrationClientFactory(t *testing.T) {
