3929319e980cd8ca5ae77b5d01e0bb531ca63b44
Jan Grant <jang@ioctl.org>
2025-03-13 16:05:25 +0000

    Add some helpers to GlobalConfiguration
    ConcreteConfiguration turns a bootstrap template into
    concrete values.
    
    Some of this is a temporary refactor until the drift
    detector can be removed.

--- operator/pkg/resources/configuration/configuration.go
+++ operator/pkg/resources/configuration/configuration.go
@@ -11,6 +11,7 @@
 package configuration
 
 import (
+	"context"
 	"crypto/md5" //nolint:gosec // this is not encrypting secure info
 	"fmt"
 	"reflect"
@@ -18,8 +19,10 @@ import (
 
 	"github.com/redpanda-data/common-go/rpadmin"
 	"github.com/redpanda-data/redpanda/src/go/rpk/pkg/config"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	vectorizedv1alpha1 "github.com/redpanda-data/redpanda-operator/operator/api/vectorized/v1alpha1"
+	"github.com/redpanda-data/redpanda-operator/operator/pkg/clusterconfiguration"
 	"github.com/redpanda-data/redpanda-operator/operator/pkg/resources/featuregates"
 )
 
@@ -39,6 +42,8 @@ type GlobalConfiguration struct {
 	ClusterConfiguration   map[string]interface{}
 	BootstrapConfiguration map[string]vectorizedv1alpha1.ClusterConfigCRDValue
 	Mode                   GlobalConfigurationMode
+	// The bootstrap will be expanded once in-process into this
+	concreteValues map[string]any
 }
 
 // For constructs a GlobalConfiguration for the given version of the cluster (considering feature gates).
@@ -97,20 +102,51 @@ func (c *GlobalConfiguration) SetAdditionalFlatProperties(
 	return c.Mode.SetAdditionalFlatProperties(c, props)
 }
 
+// ConcreteConfiguration uses the expander to completely bottom out all configuration values
+// into concrete ones, according to the supplied schema. This is performed once only,
+// with repeated calls using the cached computation.
+func (c *GlobalConfiguration) ConcreteConfiguration(ctx context.Context, reader client.Reader, namespace string, schema rpadmin.ConfigSchema) (map[string]any, error) {
+	if c.concreteValues != nil {
+		return c.concreteValues, nil
+	}
+	concreteCfg, err := clusterconfiguration.ExpandForConfiguration(ctx, reader, namespace, c.BootstrapConfiguration, schema)
+	if err != nil {
+		return nil, err
+	}
+	c.concreteValues = concreteCfg
+	return c.concreteValues, nil
+}
+
 // GetCentralizedConfigurationHash computes a hash of the centralized configuration considering only the
 // cluster properties that require a restart (this is why the schema is needed).
 func (c *GlobalConfiguration) GetCentralizedConfigurationHash(
+	ctx context.Context,
+	reader client.Reader,
+	schema rpadmin.ConfigSchema,
+	namespace string,
+) (string, error) {
+	concreteCfg, err := c.ConcreteConfiguration(ctx, reader, namespace, schema)
+	if err != nil {
+		return "", err
+	}
+
+	return c.GetCentralizedConcreteConfigurationHash(concreteCfg, schema)
+}
+
+// GetCentralizedConcreteConfigurationHash is a short-term helper to handle checking of cluster configuration.
+// It hashes only properties that require a restart.
+func (c *GlobalConfiguration) GetCentralizedConcreteConfigurationHash(
+	concreteCfg map[string]any,
 	schema rpadmin.ConfigSchema,
 ) (string, error) {
 	clone := *c
 
 	// Ignore cluster properties that don't require restart
-	clone.BootstrapConfiguration = make(map[string]vectorizedv1alpha1.ClusterConfigCRDValue)
-	// TODO: this should really do reference expansion here
-	for k, v := range c.BootstrapConfiguration {
+	clone.ClusterConfiguration = make(map[string]any)
+	for k, v := range concreteCfg {
 		// Unknown properties should be ignored as they might be user errors
 		if meta, ok := schema[k]; ok && meta.NeedsRestart {
-			clone.BootstrapConfiguration[k] = *(v.DeepCopy())
+			clone.ClusterConfiguration[k] = v
 		}
 	}
 	serialized, err := clone.Serialize()
@@ -118,7 +154,7 @@ func (c *GlobalConfiguration) GetCentralizedConfigurationHash(
 		return "", err
 	}
 	// We keep using md5 for having the same format as node hash
-	md5Hash := md5.Sum(serialized.BootstrapTemplate) //nolint:gosec // this is not encrypting secure info
+	md5Hash := md5.Sum(serialized.BootstrapFile) //nolint:gosec // this is not encrypting secure info
 	return fmt.Sprintf("%x", md5Hash), nil
 }
 
