ab16fa49809413ad32a35890a094babef9d6cf2b
Jan Grant <jang@ioctl.org>
2025-03-27 18:35:26 +0000

    v1 templating: fixups to access secured content
    Both the node config and the bootstrap config require
    templating of secrets, to support late-binding of
    values.
    
    Additionally, the client users managed by the operator
    have late-binding of usernames, which must appear in
    the `superusers` cluster config.
    
    To support this, extend the templating abilities to
    enable the construction of custom CEL expressions that
    can mutate both the node config and the serialised
    
    Files containing the template fixups, called
    `redpanda.yaml.fixups` or `bootstrap.yaml.fixups`, may
    optionally be provided. Fixups will be folded onto the
    expanded configuration if so.
    
    * move configuration construction out of configmap
    
    Drop the older "configuration" in favour of a somewhat more streamlined
    clusterconfiguration - supporting CEL-based fixups for handling late-bound
    patching of the input structures.
    
    Move away from the "combined configuration" approach to spitting
    `redpanda.yaml` and `.bootstrap.yaml` content into their own pieces.

---
+++ operator/cmd/configurator/fixup.go
@@ -0,0 +1,46 @@
+package configurator
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/cockroachdb/errors"
+	"sigs.k8s.io/yaml"
+
+	"github.com/redpanda-data/redpanda-operator/operator/pkg/clusterconfiguration"
+	"github.com/redpanda-data/redpanda-operator/operator/pkg/secrets"
+)
+
+func applyFixups[T any](ctx context.Context, config T, fn string, cloudExpander *secrets.CloudExpander) (T, error) {
+	var zero T
+	// It's harmless if the fixup file does not exist
+	fixupContent, err := os.ReadFile(fn)
+	var pe *os.PathError
+	if errors.As(err, &pe) {
+		return config, nil
+	}
+	if err != nil {
+		return zero, fmt.Errorf("cannot load fixup file %q: %w", fn, err)
+	}
+	var fs []clusterconfiguration.Fixup
+	if err := yaml.Unmarshal(fixupContent, &fs); err != nil {
+		return zero, fmt.Errorf("cannot unmarshal fixup file %q: %w", fn, err)
+	}
+	tpl := clusterconfiguration.Template[T]{
+		Content: config,
+		Fixups:  fs,
+	}
+	// Load the environment
+	env := make(map[string]string)
+	for _, ev := range os.Environ() {
+		name, val, found := strings.Cut(ev, "=")
+		if found {
+			env[name] = strings.Trim(val, "\n")
+		}
+	}
+	factory := clusterconfiguration.StdLibFactory(ctx, env, cloudExpander)
+	err = tpl.Fixup(factory)
+	return tpl.Content, err
+}
