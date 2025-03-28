f8da1f0b1f892fff316c6bb0372178b216ee5e51
Alena Varkockova <varkockova.a@gmail.com>
2025-03-28 18:43:57 +0100

    Expand cloud secrets (#569)
    * gomod: add newest pkg dependency
    
    * add cloud secrets expansion into v1 operator
    
    * add cloud secrets expansion into v1 configurator
    
    (cherry picked from commit 575887d19716da12f99bb869d73d9e9bbee90837)
    
    # Conflicts:
    #	licenses/third_party.md
    #	operator/cmd/run/run.go
    #	operator/go.mod
    #	operator/go.sum

--- operator/internal/controller/redpanda/redpanda_controller.go
+++ operator/internal/controller/redpanda/redpanda_controller.go
@@ -45,11 +45,11 @@ import (
 	"sigs.k8s.io/yaml"
 
 	"github.com/redpanda-data/redpanda-operator/charts/redpanda/v5"
+	"github.com/redpanda-data/redpanda-operator/gotohelm/helmette"
 	redpandav1alpha2 "github.com/redpanda-data/redpanda-operator/operator/api/redpanda/v1alpha2"
 	"github.com/redpanda-data/redpanda-operator/operator/cmd/syncclusterconfig"
 	internalclient "github.com/redpanda-data/redpanda-operator/operator/pkg/client"
 	"github.com/redpanda-data/redpanda-operator/operator/pkg/resources"
-	"github.com/redpanda-data/redpanda-operator/pkg/gotohelm/helmette"
 	"github.com/redpanda-data/redpanda-operator/pkg/kube"
 )
 
@@ -80,7 +80,7 @@ type gvkKey struct {
 type RedpandaReconciler struct {
 	// KubeConfig is the [kube.Config] that provides the go helm chart
 	// Kubernetes access. It should be the same config used to create client.
-	KubeConfig         kube.Config
+	KubeConfig         *kube.RESTConfig
 	Client             client.Client
 	Scheme             *runtime.Scheme
 	EventRecorder      kuberecorder.EventRecorder
@@ -360,7 +360,7 @@ func (r *RedpandaReconciler) reconcileDefluxed(ctx context.Context, rp *redpanda
 	// within the chart itself.
 	values := rp.Spec.ClusterSpec.DeepCopy()
 
-	objs, err := redpanda.Chart.Render(&r.KubeConfig, helmette.Release{
+	objs, err := redpanda.Chart.Render(r.KubeConfig, helmette.Release{
 		Namespace: rp.Namespace,
 		Name:      rp.GetHelmReleaseName(),
 		Service:   "Helm",
