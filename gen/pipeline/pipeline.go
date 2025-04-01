d269e14731eb16bfe5931d83426c37d7bfd7f8c2
Rafal Korepta <rafal.korepta@gmail.com>
2025-04-01 22:37:53 +0100

    Revert "ci: change BK pool used and add push nightlies to new ECR registry (#563)"
    This reverts commit 8f60f9d599b6ea8eea047630f726814ae86ed025.

--- gen/pipeline/pipeline.go
+++ gen/pipeline/pipeline.go
@@ -25,7 +25,7 @@ var (
 	// Known/Permitted Agent Pools
 
 	AgentsPipeLineUploader = map[string]any{"queue": "pipeline-uploader"}
-	AgentsLarge            = map[string]any{"queue": "k8s-m6id12xlarge"}
+	AgentsLarge            = map[string]any{"queue": "v6-amd64-builders-m6id"}
 )
 
 var suites = []TestSuite{
