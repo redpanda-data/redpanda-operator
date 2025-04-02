bc3c573a1b100c5618c20116d7e78e108fdf424e
Rafal Korepta <rafal.korepta@gmail.com>
2025-04-02 10:24:55 +0100

    Migrate to latest golangci-lint configuration
    (cherry picked from commit 2343b78f277a193e2ef00914f7a07bf102519517)

--- pkg/gotohelm/ast.go
+++ pkg/gotohelm/ast.go
@@ -7,7 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 
-//nolint:errcheck
+//nolint:errcheck,gosec
 package gotohelm
 
 import (
