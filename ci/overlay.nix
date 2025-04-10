4417e3964c5f4aa43b3c2720e99b7af6399de637
Chris Seto <chriskseto@gmail.com>
2025-04-10 10:27:00 -0400

    removed unused `chart-testing` dep
    During the merger of the operator and helm charts repo, the operator picked up
    a dependency on `chart-testing`.
    
    As `chart-testing` isn't used in this repository, this commit removes it.

--- ci/overlay.nix
+++ ci/overlay.nix
@@ -2,7 +2,6 @@
 }: (final: prev: {
   applyconfiguration-gen = pkgs.callPackage ./applyconfiguration-gen.nix { };
   backport = pkgs.callPackage ./backport.nix { };
-  chart-testing = pkgs.callPackage ./chart-testing.nix { };
   controller-gen = pkgs.callPackage ./controller-gen.nix { };
   crd-ref-docs = pkgs.callPackage ./crd-ref-docs.nix { };
   docker-tag-list = pkgs.callPackage ./docker-tag-list.nix { };
