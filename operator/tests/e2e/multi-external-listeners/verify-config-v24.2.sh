3aea456e7bed03c8e024fa1248025f024ef54e0c
Jan Grant <jang@ioctl.org>
2025-04-18 12:14:18 +0100

    Fix test: account for moved cluster config

--- operator/tests/e2e/multi-external-listeners/verify-config-v24.2.sh
+++ operator/tests/e2e/multi-external-listeners/verify-config-v24.2.sh
@@ -55,12 +55,9 @@ redpanda:
           name: kafka-sasl
     developer_mode: true
     auto_create_topics_enabled: true
-    cloud_storage_segment_max_upload_interval_sec: 1800
-    enable_rack_awareness: true
     fetch_reads_debounce_timeout: 10
     group_initial_rebalance_delay: 0
     group_topic_partitions: 3
-    log_segment_size: 536870912
     log_segment_size_min: 1
     storage_min_free_bytes: 10485760
     topic_partitions_per_shard: 1000
