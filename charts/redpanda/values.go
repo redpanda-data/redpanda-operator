ee4740b01e8f3deb708dcdce0da4d2cea76a7b64
Jan Grant <jang@ioctl.org>
2025-04-29 14:25:12 +0100

    v2: use fixup templating for bootstrap.yaml

--- charts/redpanda/values.go
+++ charts/redpanda/values.go
@@ -2103,19 +2103,29 @@ func (c TieredStorageConfig) CloudStorageCacheSize() *resource.Quantity {
 	return ptr.To(helmette.UnmarshalInto[resource.Quantity](value))
 }
 
+// Fixup holds details of field patching
+type Fixup struct {
+	Field string `json:"field"`
+	CEL   string `json:"cel"`
+}
+
 // Translate converts TieredStorageConfig into a map suitable for use in
 // an unexpanded `.bootstrap.yaml`.
-func (c TieredStorageConfig) Translate(creds *TieredStorageCredentials) map[string]any {
+func (c TieredStorageConfig) Translate(creds *TieredStorageCredentials) (map[string]any, []Fixup) {
 	// Clone ourselves as we're making changes.
 	config := helmette.Merge(map[string]any{}, c)
 
 	// For any values that can be specified as secrets and do not have explicit
 	// values, inject placeholders into config which will be replaced with
 	// `envsubst` in an initcontainer.
+	var fixups []Fixup
 	for _, envvar := range creds.AsEnvVars(c) {
 		key := helmette.Lower(envvar.Name[len("REDPANDA_"):])
 		// NB: No string + string support in gotohelm.
-		config[key] = fmt.Sprintf("$%s", envvar.Name)
+		fixups = append(fixups, Fixup{
+			Field: key,
+			CEL:   fmt.Sprintf(`repr(envString("%s"))`, envvar.Name),
+		})
 	}
 
 	// Expand cloud_storage_cache_size, if provided, as it can be specified as
@@ -2124,7 +2134,7 @@ func (c TieredStorageConfig) Translate(creds *TieredStorageCredentials) map[stri
 		config["cloud_storage_cache_size"] = size.Value()
 	}
 
-	return config
+	return config, fixups
 }
 
 // +gotohelm:ignore=true
