26f4c7f423a22a8aaa2526e251967a2353c6c5f5
Chris Seto <chriskseto@gmail.com>
2025-06-09 15:14:25 -0400

    ci: automatically retry acceptance and integration tests 3 times
    I've found myself in the habit of mindlessly pressing retry on any set of
    failed acceptance or integration tests. While I was initially opposed to
    automatic retries, we've all wasted ample time needing to "baby sit" PRs and
    that is only going to increase in frequency as we increase the number of PRs
    that need to be backported.
    
    This commit bites the bullet and adds 3 automatic retries to both the
    integration and acceptance test suites.
    
    (cherry picked from commit 9a982e116a46ac59c559640995ca68b8369d54f9)
    
    # Conflicts:
    #	.buildkite/testsuite.yml
    #	gen/pipeline/helpers.go
    #	gen/pipeline/pipeline.go

--- gen/pipeline/pipeline.go
+++ gen/pipeline/pipeline.go
@@ -38,11 +38,13 @@ var suites = []TestSuite{
 		Name:     "integration",
 		Required: false,
 		Timeout:  30*time.Minute + time.Hour,
+		Retry:    ptr.To(3),
 	},
 	{
 		Name:     "acceptance",
 		Required: false,
 		Timeout:  time.Hour,
+		Retry:    ptr.To(3),
 	},
 	// kuttl-v1 is currently the slowest and flakiest of our test suites. The
 	// majority of changes made aren't exercised by this suite. It's disabled
