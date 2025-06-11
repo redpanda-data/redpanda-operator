9d3c7e0d8ad2a6581ec4b19b5ff75ce33caa6541
Chris Seto <chriskseto@gmail.com>
2025-06-11 15:45:32 -0400

    kube: improve Ctl struct
    This commit improves the ergonomics of the kube.Ctl struct by allowing
    configuration with variadic options and implementing Apply. These improvements
    have been carried over to the kubetest.NewEnv function as well. Most notably,
    this allows specifying the Scheme of the testenv which makes it useable when
    testing CRDs.
    
    (cherry picked from commit c95cde915c23c1e0efe137ca6e5644984c4304d5)
    
    # Conflicts:
    #	gen/go.mod
    #	gotohelm/helmette/helm.go
    #	harpoon/go.mod
    #	harpoon/go.sum
    #	pkg/kube/generics.go

--- pkg/kube/generics.go
+++ pkg/kube/generics.go
@@ -16,29 +16,63 @@ import (
 )
 
 // ObjectList is a generic equivalent of [ObjectList].
-type ObjectList[T any] interface {
+type AddrOfObjectList[T any] interface {
 	client.ObjectList
 	*T
 }
 
-// AddrofObject is a helper type constraint for accepting a struct value that
+// AddrOfObject is a helper type constraint for accepting a struct value that
 // implements the Object interface.
+type AddrOfObject[T any] interface {
+	*T
+	client.Object
+}
+
+// AddrofObject is a copy of AddrofObject for backwards compatibility as
+// generic type aliases are currently experimental.
 type AddrofObject[T any] interface {
 	*T
 	client.Object
 }
 
+// ApplyAll is the generic equivalent of [Ctl.ApplyAll].
+func ApplyAll[T any, PT AddrOfObject[T]](ctx context.Context, ctl *Ctl, objs ...PT) error {
+	return ctl.ApplyAll(ctx, AsObjects(objs...)...)
+}
+
+// ApplyAllAndWait is the generic equivalent of [Ctl.ApplyAllAndWait].
+func ApplyAllAndWait[T any, PT AddrOfObject[T]](ctx context.Context, ctl *Ctl, cond CondFn[PT], objs ...PT) error {
+	return ctl.ApplyAllAndWait(ctx, func(obj Object, err error) (bool, error) {
+		return cond(obj.(PT), err)
+	}, AsObjects(objs...)...)
+}
+
+// Apply is the generic equivalent of [Ctl.Apply].
+func Apply[T any, PT AddrOfObject[T]](ctx context.Context, ctl *Ctl, obj PT) error {
+	return ctl.Apply(ctx, obj)
+}
+
+// ApplyAndWait is the generic equivalent of [Ctl.ApplyAndWait]
+func ApplyAndWait[T any, PT AddrOfObject[T]](ctx context.Context, ctl *Ctl, obj PT, cond CondFn[PT]) error {
+	return ctl.ApplyAndWait(ctx, obj, func(obj Object, err error) (bool, error) {
+		return cond(obj.(PT), err)
+	})
+}
+
 // List is a generic equivalent of [Ctl.List].
-func List[T any, L ObjectList[T]](ctx context.Context, ctl *Ctl, opts ...client.ListOption) (*T, error) {
+func List[T any, L AddrOfObjectList[T]](ctx context.Context, ctl *Ctl, opts ...client.ListOption) (*T, error) {
 	var list T
-	if err := ctl.client.List(ctx, L(&list), opts...); err != nil {
+	if err := ctl.List(ctx, L(&list), opts...); err != nil {
 		return nil, err
 	}
 	return &list, nil
 }
 
 // Get is a generic equivalent of [Ctl.Get].
-func Get[T any, PT AddrofObject[T]](ctx context.Context, ctl *Ctl, key ObjectKey) (*T, error) {
+//
+//	ns, err := Get[corev1.Namespace](ctx, ctl, ObjectKey{Name: "my-namespace"})
+//	pod, err := Get[corev1.Pod](ctx, ctl, ObjectKey{Namespace: "my-namespace", Name: "my-pod"})
+func Get[T any, PT AddrOfObject[T]](ctx context.Context, ctl *Ctl, key ObjectKey) (*T, error) {
 	var obj T
 	if err := ctl.client.Get(ctx, key, PT(&obj)); err != nil {
 		return nil, err
@@ -47,7 +81,7 @@ func Get[T any, PT AddrofObject[T]](ctx context.Context, ctl *Ctl, key ObjectKey
 }
 
 // Get is a generic equivalent of [Ctl.Create].
-func Create[T any, PT AddrofObject[T]](ctx context.Context, ctl *Ctl, obj T) (*T, error) {
+func Create[T any, PT AddrOfObject[T]](ctx context.Context, ctl *Ctl, obj T) (*T, error) {
 	if err := ctl.Create(ctx, PT(&obj)); err != nil {
 		return nil, err
 	}
@@ -55,7 +89,7 @@ func Create[T any, PT AddrofObject[T]](ctx context.Context, ctl *Ctl, obj T) (*T
 }
 
 // Get is a generic equivalent of [Ctl.Delete].
-func Delete[T any, PT AddrofObject[T]](ctx context.Context, ctl *Ctl, key ObjectKey) error {
+func Delete[T any, PT AddrOfObject[T]](ctx context.Context, ctl *Ctl, key ObjectKey) error {
 	obj := PT(new(T))
 	obj.SetName(key.Name)
 	obj.SetNamespace(key.Namespace)
@@ -63,6 +97,23 @@ func Delete[T any, PT AddrofObject[T]](ctx context.Context, ctl *Ctl, key Object
 	return ctl.client.Delete(ctx, obj)
 }
 
+// WaitFor is the generic equivalent of [Ctl.WaitFor].
+func WaitFor[T any, PT AddrOfObject[T]](ctx context.Context, ctl *Ctl, obj PT, cond CondFn[PT]) error {
+	return ctl.WaitFor(ctx, obj, func(obj Object, err error) (bool, error) {
+		return cond(obj.(PT), err)
+	})
+}
+
 func AsKey(obj Object) ObjectKey {
 	return ObjectKey{Namespace: obj.GetNamespace(), Name: obj.GetName()}
+}
+
+// AsObjects converts a slice of a type that implements [Object] into a slice
+// of [Object].
+func AsObjects[T any, PT AddrOfObject[T]](in ...PT) []Object {
+	out := make([]Object, len(in))
+	for i := range in {
+		out[i] = in[i]
+	}
+	return out
 }
