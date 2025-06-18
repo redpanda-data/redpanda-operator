a39bf45680f2f58bb170f2cd6614ac94da50fb30
Chris Seto <chriskseto@gmail.com>
2025-06-18 15:32:34 -0400

    redpanda: don't crash redpanda on sidecar crashes
    It's been reported that the redpanda service can be interrupted due to
    temporary outages of the Kubernetes API server. This is due to the sidecar
    container utilizing the controller-runtime's leader election which
    intentionally crashes upon failure to renew its lease.
    
    To prevent such outages from impacting redpanda, this commit implements a
    `supervisor` subcommand that wraps the provided command in a retry loop. This
    new command is used to wrap the sidecar process, effectively preventing crashes
    from impacting redpanda itself.
    
    This strategy was opted for to avoid potential errors in restarting the
    controller-runtime's manager after a crash from e.g. shared global state. It
    additionally protects redpanda other sources of crashes. Once our floor
    Kubernetes version supports [Sidecar
    containers](https://kubernetes.io/docs/concepts/workloads/pods/sidecar-containers/)
    we will utilize them instead of our own wrapper process.
    
    To aid in testing a hidden `panic-after` flag has been added to the sidecar
    container. By setting it and asserting that panics appear in the logs, we can
    assert that unexpected crashes do not affect the redpanda container.
    
    Fixes #903
    
    (cherry picked from commit c22fb9c4ba4d40b75908dde124b7220f8e30a7a2)

---
+++ operator/cmd/supervisor/supervisor_test.go
@@ -0,0 +1,119 @@
+package supervisor
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/go-logr/logr/testr"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/redpanda-data/redpanda-operator/pkg/otelutil/log"
+)
+
+// script is a bash script that logs our received signals and exits upon
+// receiving 3 in total.
+const script = `
+#!/usr/bin/env bash
+
+signal_count=0
+
+signal_handler() {
+    signal_count=$((signal_count + 1))
+    echo "received: $1"
+
+    if [[ $signal_count -eq 3 || "$1" == "SIGTERM" ]]; then
+        echo "exiting"
+        exit 0
+    fi
+}
+
+# Trap common signals
+trap 'signal_handler SIGTERM' TERM
+trap 'signal_handler SIGINT' INT
+trap 'signal_handler SIGUSR1' USR1
+trap 'signal_handler SIGUSR2' USR2
+trap 'signal_handler SIGHUP' HUP
+
+# Log AFTER traps are established.
+echo "starting"
+
+# Keep script running
+while true; do
+    sleep 0.1
+done
+`
+
+type channelWriter chan<- string
+
+func (w channelWriter) Write(p []byte) (n int, err error) {
+	for _, chunk := range bytes.Split(p, []byte{'\n'}) {
+		if len(chunk) > 0 {
+			w <- string(chunk)
+		}
+	}
+	return len(p), nil
+}
+
+func TestRunner(t *testing.T) {
+	run := func(ctx context.Context, signals chan os.Signal) <-chan string {
+		stdout := make(chan string)
+
+		go func() {
+			r := Runner{
+				Command:     []string{"bash", "-c", script},
+				GracePeriod: 5 * time.Second,
+				Stdout:      channelWriter(stdout),
+				Signals:     signals,
+			}
+
+			cont := r.Run(ctx)
+
+			stdout <- fmt.Sprintf("%v", cont)
+
+			close(stdout)
+		}()
+
+		return stdout
+	}
+
+	t.Run("signal forwarding", func(t *testing.T) {
+		ctx := log.IntoContext(t.Context(), testr.New(t))
+		signals := make(chan os.Signal)
+
+		stdout := run(ctx, signals)
+
+		require.Equal(t, "starting", <-stdout)
+
+		for i := 0; i < 3; i++ {
+			signals <- syscall.SIGHUP
+			assert.Equal(t, "received: SIGHUP", <-stdout)
+		}
+
+		require.Equal(t, "exiting", <-stdout)
+		require.Equal(t, "true", <-stdout)
+	})
+
+	t.Run("graceful termination", func(t *testing.T) {
+		ctx := log.IntoContext(t.Context(), testr.New(t))
+		ctx, cancel := context.WithCancel(ctx)
+		signals := make(chan os.Signal)
+
+		stdout := run(ctx, signals)
+
+		require.Equal(t, "starting", <-stdout)
+
+		cancel()
+
+		require.Equal(t, "received: SIGTERM", <-stdout)
+		require.Equal(t, "exiting", <-stdout)
+
+		// Context cancellation doesn't result in requesting of a retry.
+		require.Equal(t, "false", <-stdout)
+	})
+}
--- operator/go.mod
+++ operator/go.mod
@@ -50,6 +50,7 @@ require (
 	go.uber.org/zap v1.27.0
 	golang.org/x/exp v0.0.0-20240904232852-e7e105dedf7e
 	golang.org/x/sync v0.14.0
+	golang.org/x/time v0.11.0
 	golang.org/x/tools v0.29.0
 	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.1
@@ -309,7 +310,6 @@ require (
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/term v0.32.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
-	golang.org/x/time v0.11.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.5.0 // indirect
 	google.golang.org/api v0.203.0 // indirect
 	google.golang.org/genproto v0.0.0-20241015192408-796eee8c2d53 // indirect
