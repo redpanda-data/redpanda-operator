cfb8827261fdb83cb3ebf4bdd81768a75ae4874d
Chris Seto <chriskseto@gmail.com>
2025-07-17 12:40:54 -0400

    operator: add annotation feature flag system
    This commit adds a generic annotation based feature flagging system at
    `operator/pkg/feature` to consolidate the existing flags and reduce boiler
    plate for future flags.
    
    Notably, this package includes a function `SetDefaults` which will update the
    annotations of an object with all the default values of any flags. This makes
    it trivial for human operators to identify the set of available "knobs" when
    debugging a cluster.

---
+++ operator/pkg/feature/flag_test.go
@@ -0,0 +1,96 @@
+package feature
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFlags(t *testing.T) {
+	t.Parallel()
+
+	for _, bundle := range bundles {
+		flags := make(map[string]struct{}, len(bundle))
+		for _, flag := range bundle {
+			// All defaults should successfully parse
+			_, err := flag.Parse(flag.Default)
+			assert.NoErrorf(t, err, "%q's default failed to parse: %q", flag.Key, flag.Default)
+
+			// All flag names must be unique
+			assert.NotContains(t, flag.Key, "%q is a non-unique flag", flag.Key)
+			flags[flag.Key] = struct{}{}
+
+		}
+	}
+}
+
+func TestSetDefaults(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		In      Annotations
+		Out     Annotations
+		Changed bool
+	}{
+		{
+			In: Annotations{},
+			Out: Annotations{
+				Annotations: map[string]string{
+					"cluster.redpanda.com/managed":                           "true",
+					"operator.redpanda.com/config-sync-mode":                 "additive",
+					"operator.redpanda.com/restart-cluster-on-config-change": "false",
+				},
+			},
+			Changed: true,
+		},
+		{
+			In: Annotations{
+				Annotations: map[string]string{
+					"cluster.redpanda.com/managed":                           "true",
+					"operator.redpanda.com/config-sync-mode":                 "additive",
+					"operator.redpanda.com/restart-cluster-on-config-change": "false",
+				},
+			},
+			Out: Annotations{
+				Annotations: map[string]string{
+					"cluster.redpanda.com/managed":                           "true",
+					"operator.redpanda.com/config-sync-mode":                 "additive",
+					"operator.redpanda.com/restart-cluster-on-config-change": "false",
+				},
+			},
+			Changed: false,
+		},
+		{
+			In: Annotations{
+				Annotations: map[string]string{
+					"operator.redpanda.com/config-sync-mode":                 "",
+					"cluster.redpanda.com/managed":                           "",
+					"operator.redpanda.com/restart-cluster-on-config-change": "",
+				},
+			},
+			Out: Annotations{
+				Annotations: map[string]string{
+					"cluster.redpanda.com/managed":                           "",
+					"operator.redpanda.com/config-sync-mode":                 "",
+					"operator.redpanda.com/restart-cluster-on-config-change": "",
+				},
+			},
+			Changed: false,
+		},
+	}
+
+	for _, tc := range cases {
+		SetDefaults(t.Context(), V2Flags, &tc.In)
+		assert.Equal(t, tc.Out, tc.In)
+	}
+}
+
+type Annotations struct{ Annotations map[string]string }
+
+func (a *Annotations) GetAnnotations() map[string]string {
+	return a.Annotations
+}
+
+func (a *Annotations) SetAnnotations(annos map[string]string) {
+	a.Annotations = annos
+}
