7dd3bae3fe756c40cf721c11c1a4e9be20b48056
Rafal Korepta <rafal.korepta@gmail.com>
2025-11-17 16:58:59 +0100

    pkg: Copy k3d package from operator go module
    This package will be used in next change in harpoon go module to
    programmatically controll k3d cluster (adding and removing kubernetes nodes).
    
    Small addition from main branch was added to k3d package.

---
+++ pkg/k3d/k3d_test.go
@@ -0,0 +1,59 @@
+// Copyright 2025 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+package k3d
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/cockroachdb/errors"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/redpanda-data/redpanda-operator/pkg/testutil"
+)
+
+func TestIntegrationMultiInstance(t *testing.T) {
+	testutil.SkipIfNotIntegration(t)
+
+	clusters := 3
+
+	var running sync.WaitGroup
+	running.Add(clusters)
+
+	errCh := make(chan error, clusters)
+	done := make(chan struct{})
+	defer func() {
+		close(done)
+		running.Wait()
+	}()
+
+	for i := 0; i < clusters; i++ {
+		go func() {
+			defer running.Done()
+
+			name := fmt.Sprintf("cluster-%d", i)
+			_, err := GetOrCreate(name, WithAgents(1))
+			defer forceCleanup(name)
+			errCh <- err
+
+			<-done
+		}()
+	}
+
+	errs := []error{}
+	for i := 0; i < clusters; i++ {
+		if err := <-errCh; err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	assert.NoError(t, errors.Join(errs...))
+}
