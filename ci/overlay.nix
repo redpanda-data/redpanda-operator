0e13fd0b89c5d25008141504664276a9131f3a9b
Travis Bischel <travis.bischel+github@gmail.com>
2026-05-11 09:24:24 -0600

    Override pkgs.go_1_25 to 1.25.10 (CI go-licenses + stdlib fix)

--- ci/overlay.nix
+++ ci/overlay.nix
@@ -12,4 +12,15 @@
   kuttl = pkgs.callPackage ./kuttl.nix { };
   setup-envtest = pkgs.callPackage ./setup-envtest.nix { };
   vcluster = pkgs.callPackage ./vcluster.nix { };
+
+  # Pin Go 1.25.10 (stdlib fix for CVE-2026-33811 / CVE-2026-33814 / CVE-2026-39836).
+  # nixpkgs-unstable currently provides 1.25.7; overriding source so we don't have
+  # to bump nixpkgs (which would pull in unrelated changes).
+  go_1_25 = prev.go_1_25.overrideAttrs (oldAttrs: {
+    version = "1.25.10";
+    src = prev.fetchurl {
+      url = "https://go.dev/dl/go1.25.10.src.tar.gz";
+      hash = "sha256-IM8EqS5a+ZdI40G8iZb6KAkMmsmHZfoRXsXd9B169B0=";
+    };
+  });
 })
