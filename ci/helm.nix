773c9886a126051f53a17e4bf30783014ba20023
Chris Seto <chriskseto@gmail.com>
2026-08-04 13:36:01 -0400

    nix: correct go install to 1.26.5
    The go package installed by nix was 1.26.1, not 1.26.5 as reported by
    go.work/go.mod. This was due to the default value of `GOTOOLCHAIN` being
    set to `auto` which causes go to fetch the listed toolchain version.
    
    This was discovered because we manually set `GOROOT` which causes a
    subtle mismatch between the go binary version and GOROOT's version.
    
    Correct the installed version to go 1.26.1 by bumping nixpkg's sha. This
    incidentally upgraded a handful of other packages. Most notably was
    `helm`, which upgraded to v4. As we're not currently working with v4,
    additionally pin to the previously installed version.

--- ci/helm.nix
+++ ci/helm.nix
@@ -1,27 +1,24 @@
 { pkgs
 , stdenv
-, fetchzip
+}:
+{ version
+, src
+  # When true, the binary is installed as `helm-${version}` instead of `helm` so
+  # that multiple versions may coexist on $PATH.
+, versionSuffix ? false
 }:
 let
   pname = "helm";
-  version = "3.10.3";
-  src = {
-    aarch64-darwin = fetchzip {
-      url = "https://get.helm.sh/helm-v${version}-darwin-arm64.tar.gz";
-      hash = "sha256-3W/piPZvkyrGOLCgghn7j9CgNxAVvWn1kwFb8Von9Ko=";
-    };
-    x86_64-linux = fetchzip {
-      url = "https://get.helm.sh/helm-v${version}-linux-amd64.tar.gz";
-      hash = "sha256-XAtiT7vaSBrfrj03gbcQUmUMQSZ9+5nymxfVSOnQ+sM=";
-    };
-  }.${stdenv.system} or (throw "${pname}-${version}: ${stdenv.system} is unsupported.");
+  binary = if versionSuffix then "helm-${version}" else "helm";
 in
 stdenv.mkDerivation {
-  inherit pname version src;
+  inherit pname version;
+
+  src = src.${stdenv.system} or (throw "${pname}-${version}: ${stdenv.system} is unsupported.");
 
   installPhase = ''
     mkdir -p "$out/bin"
-    cp "$src/helm" "$out/bin/helm-${version}"
-    chmod 755 "$out/bin/helm-${version}"
+    cp "$src/helm" "$out/bin/${binary}"
+    chmod 755 "$out/bin/${binary}"
   '';
 }
