6dad08580dbb7ff746711836fc394f5c66f67d85
Chris Seto <chriskseto@gmail.com>
2026-08-04 13:36:01 -0400

    nix: correct go install to 1.26.5
    The go package installed by nix was 1.26.1, not 1.26.5 as reported by
    go.work/go.mod. This was due to the default value of `GOTOOLCHAIN` being
    set to `auto` which causes go to fetch the listed toolchain version.
    
    This was discovered because we manually set `GOROOT` which causes a
    subtle mismatch between the go binary version and GOROOT's version.
    
    Correct the installed version to go 1.26.1 by bumping nixpkg's sha. This
    incidentally upgraded a handful of other packages. Most notably was
    `helm`, which upgraded to v4. As we're not currently working with v4,
    additionally pin to the previously installed version.
    
    (cherry picked from commit 773c9886a126051f53a17e4bf30783014ba20023)

--- ci/overlay.nix
+++ ci/overlay.nix
@@ -1,5 +1,9 @@
 { pkgs
-}: (final: prev: {
+}:
+let
+  mkHelm = pkgs.callPackage ./helm.nix { };
+in
+(final: prev: {
   backport = pkgs.callPackage ./backport.nix { };
   bk = pkgs.callPackage ./bk.nix { };
   code-generator = pkgs.callPackage ./code-generator.nix { };
@@ -7,10 +11,43 @@
   crd-ref-docs = pkgs.callPackage ./crd-ref-docs.nix { };
   docker-tag-list = pkgs.callPackage ./docker-tag-list.nix { };
   goverter = pkgs.callPackage ./goverter.nix { };
-  helm-3-10-3 = pkgs.callPackage ./helm.nix { };
   kuttl = pkgs.callPackage ./kuttl.nix { };
   licenseupdater = pkgs.callPackage ./licenseupdater.nix { };
   rp-controller-gen = pkgs.callPackage ./rp-controller-gen.nix { };
   setup-envtest = pkgs.callPackage ./setup-envtest.nix { };
   vcluster = pkgs.callPackage ./vcluster.nix { };
+  # The hashes below are the unpacked (NAR) hashes of the release tarballs, as
+  # required by fetchzip. To compute one for a new version/platform:
+  #
+  #   nix hash convert --hash-algo sha256 --to sri \
+  #     "$(nix-prefetch-url --unpack --type sha256 https://get.helm.sh/helm-v<VERSION>-<PLATFORM>.tar.gz)"
+  #
+  # Where <PLATFORM> is one of darwin-arm64 or linux-amd64.
+  helm-3-10-3 = mkHelm {
+    version = "3.10.3";
+    versionSuffix = true;
+    src = {
+      aarch64-darwin = pkgs.fetchzip {
+        url = "https://get.helm.sh/helm-v3.10.3-darwin-arm64.tar.gz";
+        hash = "sha256-3W/piPZvkyrGOLCgghn7j9CgNxAVvWn1kwFb8Von9Ko=";
+      };
+      x86_64-linux = pkgs.fetchzip {
+        url = "https://get.helm.sh/helm-v3.10.3-linux-amd64.tar.gz";
+        hash = "sha256-XAtiT7vaSBrfrj03gbcQUmUMQSZ9+5nymxfVSOnQ+sM=";
+      };
+    };
+  };
+  helm-3-19-1 = mkHelm {
+    version = "3.19.1";
+    src = {
+      aarch64-darwin = pkgs.fetchzip {
+        url = "https://get.helm.sh/helm-v3.19.1-darwin-arm64.tar.gz";
+        hash = "sha256-MrWws7eObrZUpP/xU1hElbSloa8GZFwI4rwENNP9ez8=";
+      };
+      x86_64-linux = pkgs.fetchzip {
+        url = "https://get.helm.sh/helm-v3.19.1-linux-amd64.tar.gz";
+        hash = "sha256-8feTouvv+I89Lqsg732jQZBxUcwVqr/c8RuDUYpoUK4=";
+      };
+    };
+  };
 })
