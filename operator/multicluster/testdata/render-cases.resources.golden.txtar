redpanda-data/redpanda-operator

commit 132f4b514093772e1c253a5dfaeb860085dd56e6

Upgrade go and controller-runtime versions (#1279)
* Upgrade go and controller-runtime versions

* suppress linter issues

* Bump to 1.26.0 and simplify some nix dependencies for cache size

* Regen toolset

operator/chart/servicemonitor.go
@@ -41,10 +41,14 @@ func ServiceMonitor(dot *helmette.Dot) *monitoringv1.ServiceMonitor {
 				{
 					Port:   "https",
 					Path:   "/metrics",
-					Scheme: "https",
-					TLSConfig: &monitoringv1.TLSConfig{
-						SafeTLSConfig: monitoringv1.SafeTLSConfig{
-							InsecureSkipVerify: ptr.To(true),
+					Scheme: ptr.To(monitoringv1.SchemeHTTPS),
+					HTTPConfigWithProxyAndTLSFiles: monitoringv1.HTTPConfigWithProxyAndTLSFiles{
+						HTTPConfigWithTLSFiles: monitoringv1.HTTPConfigWithTLSFiles{
+							TLSConfig: &monitoringv1.TLSConfig{
+								SafeTLSConfig: monitoringv1.SafeTLSConfig{
+									InsecureSkipVerify: ptr.To(true),
+								},
+							},
 						},
 					},
 					BearerTokenFile: "/var/run/secrets/kubernetes.io/serviceaccount/token",

operator/chart/templates/_servicemonitor.go.tpl
@@ -12,7 +12,7 @@
 {{- break -}}
 {{- end -}}
 {{- $_is_returning = true -}}
-{{- (dict "r" (mustMergeOverwrite (dict "metadata" (dict) "spec" (dict "endpoints" (coalesce nil) "selector" (dict) "namespaceSelector" (dict))) (mustMergeOverwrite (dict) (dict "kind" "ServiceMonitor" "apiVersion" "monitoring.coreos.com/v1")) (dict "metadata" (mustMergeOverwrite (dict) (dict "name" (get (fromJson (include "operator.cleanForK8sWithSuffix" (dict "a" (list (get (fromJson (include "operator.Fullname" (dict "a" (list $dot)))) "r") "metrics-monitor")))) "r") "labels" (get (fromJson (include "operator.Labels" (dict "a" (list $dot)))) "r") "namespace" $dot.Release.Namespace "annotations" $values.annotations)) "spec" (mustMergeOverwrite (dict "endpoints" (coalesce nil) "selector" (dict) "namespaceSelector" (dict)) (dict "endpoints" (list (mustMergeOverwrite (dict) (dict "port" "https" "path" "/metrics" "scheme" "https" "tlsConfig" (mustMergeOverwrite (dict "ca" (dict) "cert" (dict)) (mustMergeOverwrite (dict "ca" (dict) "cert" (dict)) (dict "insecureSkipVerify" true)) (dict)) "bearerTokenFile" "/var/run/secrets/kubernetes.io/serviceaccount/token"))) "namespaceSelector" (mustMergeOverwrite (dict) (dict "matchNames" (list $dot.Release.Namespace))) "selector" (mustMergeOverwrite (dict) (dict "matchLabels" (get (fromJson (include "operator.Labels" (dict "a" (list $dot)))) "r")))))))) | toJson -}}
+{{- (dict "r" (mustMergeOverwrite (dict "metadata" (dict) "spec" (dict "endpoints" (coalesce nil) "selector" (dict) "namespaceSelector" (dict))) (mustMergeOverwrite (dict) (dict "kind" "ServiceMonitor" "apiVersion" "monitoring.coreos.com/v1")) (dict "metadata" (mustMergeOverwrite (dict) (dict "name" (get (fromJson (include "operator.cleanForK8sWithSuffix" (dict "a" (list (get (fromJson (include "operator.Fullname" (dict "a" (list $dot)))) "r") "metrics-monitor")))) "r") "labels" (get (fromJson (include "operator.Labels" (dict "a" (list $dot)))) "r") "namespace" $dot.Release.Namespace "annotations" $values.annotations)) "spec" (mustMergeOverwrite (dict "endpoints" (coalesce nil) "selector" (dict) "namespaceSelector" (dict)) (dict "endpoints" (list (mustMergeOverwrite (dict) (mustMergeOverwrite (dict) (mustMergeOverwrite (dict) (dict "tlsConfig" (mustMergeOverwrite (dict "ca" (dict) "cert" (dict)) (mustMergeOverwrite (dict "ca" (dict) "cert" (dict)) (dict "insecureSkipVerify" true)) (dict)))) (dict)) (dict "port" "https" "path" "/metrics" "scheme" "HTTPS" "bearerTokenFile" "/var/run/secrets/kubernetes.io/serviceaccount/token"))) "namespaceSelector" (mustMergeOverwrite (dict) (dict "matchNames" (list $dot.Release.Namespace))) "selector" (mustMergeOverwrite (dict) (dict "matchLabels" (get (fromJson (include "operator.Labels" (dict "a" (list $dot)))) "r")))))))) | toJson -}}
 {{- break -}}
 {{- end -}}
 {{- end -}}

operator/multicluster/servicemonitor.go
@@ -24,15 +24,14 @@ func ServiceMonitor(state *RenderState) *monitoringv1.ServiceMonitor {
 	}
 
 	endpoint := monitoringv1.Endpoint{
-		Interval:    state.Values.Monitoring.ScrapeInterval,
-		Path:        "/public_metrics",
-		Port:        "admin",
-		EnableHttp2: state.Values.Monitoring.EnableHTTP2,
-		Scheme:      "http",
+		Interval: state.Values.Monitoring.ScrapeInterval,
+		Path:     "/public_metrics",
+		Port:     "admin",
+		Scheme:   ptr.To(monitoringv1.SchemeHTTP),
 	}
 
 	if state.Values.Listeners.Admin.TLS.IsEnabled(&state.Values.TLS) || state.Values.Monitoring.TLSConfig != nil {
-		endpoint.Scheme = "https"
+		endpoint.Scheme = ptr.To(monitoringv1.SchemeHTTPS)
 		endpoint.TLSConfig = state.Values.Monitoring.TLSConfig
 
 		if endpoint.TLSConfig == nil {

operator/chart/testdata/template-cases.golden.txtar
@@ -9083,7 +9083,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -32389,7 +32389,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -37873,7 +37873,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -43329,7 +43329,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -45218,7 +45218,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -47106,7 +47106,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -51408,7 +51408,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -54884,7 +54884,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -58098,7 +58098,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -60321,7 +60321,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -63691,7 +63691,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -65746,7 +65746,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}
@@ -67762,7 +67762,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: https
+    scheme: HTTPS
     tlsConfig:
       ca: {}
       cert: {}

charts/connectors/testdata/template-cases.golden.txtar
@@ -203,9 +203,7 @@ spec:
   namespaceSelector:
     any: true
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -2064,9 +2062,7 @@ spec:
   namespaceSelector:
     any: true
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -2403,9 +2399,7 @@ spec:
   namespaceSelector:
     any: true
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -2618,9 +2612,7 @@ spec:
     - jKf
     - p
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -4214,9 +4206,7 @@ spec:
   namespaceSelector:
     any: true
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -4908,9 +4898,7 @@ spec:
     - klNT12U
     - 9e
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -5449,9 +5437,7 @@ spec:
   namespaceSelector:
     any: true
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -6253,9 +6239,7 @@ spec:
     matchNames:
     - FKCzSYm7gaXuLQ
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -7648,9 +7632,7 @@ spec:
   namespaceSelector:
     any: true
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -8125,9 +8107,7 @@ spec:
   namespaceSelector:
     any: true
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -8520,9 +8500,7 @@ spec:
   namespaceSelector:
     any: true
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -8604,9 +8582,7 @@ spec:
     matchNames:
     - UCZpu
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -8680,9 +8656,7 @@ spec:
   namespaceSelector:
     any: true
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:
@@ -9014,9 +8988,7 @@ spec:
     matchNames:
     - WZxK8iNK2gdU
   podMetricsEndpoints:
-  - bearerTokenSecret:
-      key: ""
-    path: /
+  - path: /
     port: prometheus
   selector:
     matchLabels:

charts/redpanda/chart/templates/_servicemonitor.go.tpl
@@ -10,9 +10,9 @@
 {{- (dict "r" (coalesce nil)) | toJson -}}
 {{- break -}}
 {{- end -}}
-{{- $endpoint := (mustMergeOverwrite (dict) (dict "interval" $state.Values.monitoring.scrapeInterval "path" "/public_metrics" "port" "admin" "enableHttp2" $state.Values.monitoring.enableHttp2 "scheme" "http")) -}}
+{{- $endpoint := (mustMergeOverwrite (dict) (dict "interval" $state.Values.monitoring.scrapeInterval "path" "/public_metrics" "port" "admin" "scheme" "HTTP")) -}}
 {{- if (or (get (fromJson (include "redpanda.InternalTLS.IsEnabled" (dict "a" (list $state.Values.listeners.admin.tls $state.Values.tls)))) "r") (ne (toJson $state.Values.monitoring.tlsConfig) "null")) -}}
-{{- $_ := (set $endpoint "scheme" "https") -}}
+{{- $_ := (set $endpoint "scheme" "HTTPS") -}}
 {{- $_ := (set $endpoint "tlsConfig" $state.Values.monitoring.tlsConfig) -}}
 {{- if (eq (toJson $endpoint.tlsConfig) "null") -}}
 {{- $_ := (set $endpoint "tlsConfig" (mustMergeOverwrite (dict "ca" (dict) "cert" (dict)) (mustMergeOverwrite (dict "ca" (dict) "cert" (dict)) (dict "insecureSkipVerify" true)) (dict))) -}}

charts/redpanda/servicemonitor.go
@@ -24,15 +24,14 @@ func ServiceMonitor(state *RenderState) *monitoringv1.ServiceMonitor {
 	}
 
 	endpoint := monitoringv1.Endpoint{
-		Interval:    state.Values.Monitoring.ScrapeInterval,
-		Path:        "/public_metrics",
-		Port:        "admin",
-		EnableHttp2: state.Values.Monitoring.EnableHTTP2,
-		Scheme:      "http",
+		Interval: state.Values.Monitoring.ScrapeInterval,
+		Path:     "/public_metrics",
+		Port:     "admin",
+		Scheme:   ptr.To(monitoringv1.SchemeHTTP),
 	}
 
 	if state.Values.Listeners.Admin.TLS.IsEnabled(&state.Values.TLS) || state.Values.Monitoring.TLSConfig != nil {
-		endpoint.Scheme = "https"
+		endpoint.Scheme = ptr.To(monitoringv1.SchemeHTTPS)
 		endpoint.TLSConfig = state.Values.Monitoring.TLSConfig
 
 		if endpoint.TLSConfig == nil {

commit c49d0239720e84d144344a2a8cc44fd8d94c9c55

operator: render ServiceMonitor scheme as lowercase
The operator chart's ServiceMonitor and the StretchCluster per-pool
ServiceMonitors both emitted `scheme: HTTP` / `scheme: HTTPS`
(uppercase). Older prometheus-operator CRDs only allow `http` / `https`
in the Scheme enum and reject the object at apply time:

  ServiceMonitor.monitoring.coreos.com "..." is invalid:
  spec.endpoints[0].scheme: Unsupported value: "HTTPS":
    supported values: "http", "https"

Same regression as #1511 (introduced by PR #1279's prometheus-operator
bump, which changed Endpoint.Scheme to typed constants whose values are
uppercase); that fix covered the redpanda and console charts but missed
these two sites. Apply the same fix: cast lowercase string literals to
the typed Scheme instead of using the upstream constants.

The multicluster ServiceMonitor renderer also had no golden coverage:
the `monitoring` render case set `spec.monitoring.enabled` on the
StretchCluster, but that field only exists per-pool, so the lenient
test decoder silently dropped it and no golden ever contained a
ServiceMonitor. Move `monitoring` onto the pools in the `monitoring`
and `full-featured` cases; the resources golden now locks in the
lowercase scheme.

Regenerated:
  - operator/chart/templates/_servicemonitor.go.tpl
  - operator/chart/testdata/template-cases.golden.txtar
  - operator/multicluster/testdata/render-cases.resources.golden.txtar

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

operator/chart/servicemonitor.go
@@ -25,10 +25,15 @@ func ServiceMonitor(dot *helmette.Dot) *monitoringv1.ServiceMonitor {
 		return nil
 	}
 
+	// Render the scheme as lowercase. The prometheus-operator typed constants
+	// (monitoringv1.SchemeHTTP / SchemeHTTPS) resolve to "HTTP" / "HTTPS",
+	// which older prometheus-operator CRDs reject with
+	// `spec.endpoints[0].scheme: Unsupported value`. Lowercase works on every
+	// version. See #1511.
 	endpoint := monitoringv1.Endpoint{
 		Port:   "https",
 		Path:   "/metrics",
-		Scheme: ptr.To(monitoringv1.SchemeHTTPS),
+		Scheme: ptr.To(monitoringv1.Scheme("https")),
 		HTTPConfigWithProxyAndTLSFiles: monitoringv1.HTTPConfigWithProxyAndTLSFiles{
 			HTTPConfigWithTLSFiles: monitoringv1.HTTPConfigWithTLSFiles{
 				TLSConfig: &monitoringv1.TLSConfig{

operator/chart/templates/_servicemonitor.go.tpl
@@ -11,7 +11,7 @@
 {{- (dict "r" (coalesce nil)) | toJson -}}
 {{- break -}}
 {{- end -}}
-{{- $endpoint := (mustMergeOverwrite (dict) (mustMergeOverwrite (dict) (mustMergeOverwrite (dict) (dict "tlsConfig" (mustMergeOverwrite (dict "ca" (dict) "cert" (dict)) (mustMergeOverwrite (dict "ca" (dict) "cert" (dict)) (dict "insecureSkipVerify" true)) (dict)))) (dict)) (dict "port" "https" "path" "/metrics" "scheme" "HTTPS" "bearerTokenFile" "/var/run/secrets/kubernetes.io/serviceaccount/token")) -}}
+{{- $endpoint := (mustMergeOverwrite (dict) (mustMergeOverwrite (dict) (mustMergeOverwrite (dict) (dict "tlsConfig" (mustMergeOverwrite (dict "ca" (dict) "cert" (dict)) (mustMergeOverwrite (dict "ca" (dict) "cert" (dict)) (dict "insecureSkipVerify" true)) (dict)))) (dict)) (dict "port" "https" "path" "/metrics" "scheme" (toString "https") "bearerTokenFile" "/var/run/secrets/kubernetes.io/serviceaccount/token")) -}}
 {{- if (ne $values.monitoring.scrapeInterval "") -}}
 {{- $_ := (set $endpoint "interval" (toString $values.monitoring.scrapeInterval)) -}}
 {{- end -}}

operator/multicluster/servicemonitor.go
@@ -46,15 +46,20 @@ func serviceMonitorForPool(state *RenderState, pool *redpandav1alpha2.RedpandaBr
 		interval = monitoringv1.Duration(*mon.ScrapeInterval)
 	}
 
+	// Render the scheme as lowercase. The prometheus-operator typed constants
+	// (monitoringv1.SchemeHTTP / SchemeHTTPS) resolve to "HTTP" / "HTTPS",
+	// which older prometheus-operator CRDs reject with
+	// `spec.endpoints[0].scheme: Unsupported value`. Lowercase works on every
+	// version. See #1511.
 	endpoint := monitoringv1.Endpoint{
 		Interval: interval,
 		Path:     publicMetricsPath,
 		Port:     internalAdminAPIPortName,
-		Scheme:   ptr.To(monitoringv1.SchemeHTTP),
+		Scheme:   ptr.To(monitoringv1.Scheme("http")),
 	}
 
 	if pool.Spec.IsAdminTLSEnabled() || mon.TLSConfig != nil {
-		endpoint.Scheme = ptr.To(monitoringv1.SchemeHTTPS)
+		endpoint.Scheme = ptr.To(monitoringv1.Scheme("https"))
 
 		// Use custom TLS config if provided, otherwise fall back to insecure skip verify.
 		if mon.TLSConfig != nil {

operator/chart/testdata/template-cases.golden.txtar
@@ -11497,7 +11497,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -40711,7 +40711,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -47318,7 +47318,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -53883,7 +53883,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -56143,7 +56143,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -58218,7 +58218,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -63091,7 +63091,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -66955,7 +66955,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -70712,7 +70712,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -73311,7 +73311,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -77247,7 +77247,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -79674,7 +79674,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -82064,7 +82064,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -107617,7 +107617,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -111035,7 +111035,7 @@ spec:
     interval: 15s
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -112700,7 +112700,7 @@ spec:
   - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}
@@ -114364,7 +114364,7 @@ spec:
     interval: 60s
     path: /metrics
     port: https
-    scheme: HTTPS
+    scheme: https
     tlsConfig:
       ca: {}
       cert: {}

operator/multicluster/testdata/render-cases.resources.golden.txtar
@@ -11778,7 +11778,7 @@
       app.kubernetes.io/instance: full-featured
       app.kubernetes.io/managed-by: redpanda-operator
       app.kubernetes.io/name: redpanda
-      monitoring.redpanda.com/enabled: "false"
+      monitoring.redpanda.com/enabled: "true"
       team: data-platform
     name: full-featured
     namespace: full-featured
@@ -11813,6 +11813,60 @@
     type: ClusterIP
   status:
     loadBalancer: {}
+- apiVersion: monitoring.coreos.com/v1
+  kind: ServiceMonitor
+  metadata:
+    labels:
+      app.kubernetes.io/cluster-name: test
+      app.kubernetes.io/component: redpanda
+      app.kubernetes.io/instance: full-featured
+      app.kubernetes.io/managed-by: redpanda-operator
+      app.kubernetes.io/name: redpanda
+      team: data-platform
+    name: full-featured-pool-cold
+    namespace: full-featured
+  spec:
+    endpoints:
+    - path: /public_metrics
+      port: admin
+      scheme: https
+      tlsConfig:
+        ca: {}
+        cert: {}
+        insecureSkipVerify: true
+    namespaceSelector: {}
+    selector:
+      matchLabels:
+        app.kubernetes.io/instance: full-featured
+        app.kubernetes.io/name: redpanda
+        monitoring.redpanda.com/enabled: "true"
+- apiVersion: monitoring.coreos.com/v1
+  kind: ServiceMonitor
+  metadata:
+    labels:
+      app.kubernetes.io/cluster-name: test
+      app.kubernetes.io/component: redpanda
+      app.kubernetes.io/instance: full-featured
+      app.kubernetes.io/managed-by: redpanda-operator
+      app.kubernetes.io/name: redpanda
+      team: data-platform
+    name: full-featured-pool-hot
+    namespace: full-featured
+  spec:
+    endpoints:
+    - path: /public_metrics
+      port: admin
+      scheme: https
+      tlsConfig:
+        ca: {}
+        cert: {}
+        insecureSkipVerify: true
+    namespaceSelector: {}
+    selector:
+      matchLabels:
+        app.kubernetes.io/instance: full-featured
+        app.kubernetes.io/name: redpanda
+        monitoring.redpanda.com/enabled: "true"
 - apiVersion: v1
   data:
     .bootstrap.json.in: '{"audit_enabled":"true","cloud_storage_bucket":"\"prod-tiered\"","cloud_storage_cache_size":"21474836480","cloud_storage_credentials_source":"\"aws_instance_metadata\"","cloud_storage_enable_remote_read":"true","cloud_storage_enable_remote_write":"true","cloud_storage_enabled":"true","cloud_storage_region":"\"us-east-1\"","compacted_log_segment_size":"67108864","default_topic_replications":"3","enable_rack_awareness":"false","enable_sasl":"true","kafka_connection_rate_limit":"1000","kafka_enable_authorization":"true","log_segment_size":"134217728","log_segment_size_max":"268435456","log_segment_size_min":"16777216","max_compacted_log_segment_size":"536870912","storage_min_free_bytes":"5368709120","superusers":"[\"kubernetes-controller\"]"}'
@@ -13200,7 +13254,7 @@
       app.kubernetes.io/instance: full-featured
       app.kubernetes.io/managed-by: redpanda-operator
       app.kubernetes.io/name: redpanda
-      monitoring.redpanda.com/enabled: "false"
+      monitoring.redpanda.com/enabled: "true"
       team: data-platform
     name: full-featured-pool-cold-0
     namespace: full-featured
@@ -13247,7 +13301,7 @@
       app.kubernetes.io/instance: full-featured
       app.kubernetes.io/managed-by: redpanda-operator
       app.kubernetes.io/name: redpanda
-      monitoring.redpanda.com/enabled: "false"
+      monitoring.redpanda.com/enabled: "true"
       team: data-platform
     name: full-featured-pool-cold-1
     namespace: full-featured
@@ -13294,7 +13348,7 @@
       app.kubernetes.io/instance: full-featured
       app.kubernetes.io/managed-by: redpanda-operator
       app.kubernetes.io/name: redpanda
-      monitoring.redpanda.com/enabled: "false"
+      monitoring.redpanda.com/enabled: "true"
       team: data-platform
     name: full-featured-pool-hot-0
     namespace: full-featured
@@ -13341,7 +13395,7 @@
       app.kubernetes.io/instance: full-featured
       app.kubernetes.io/managed-by: redpanda-operator
       app.kubernetes.io/name: redpanda
-      monitoring.redpanda.com/enabled: "false"
+      monitoring.redpanda.com/enabled: "true"
       team: data-platform
     name: full-featured-pool-hot-1
     namespace: full-featured
@@ -13388,7 +13442,7 @@
       app.kubernetes.io/instance: full-featured
       app.kubernetes.io/managed-by: redpanda-operator
       app.kubernetes.io/name: redpanda
-      monitoring.redpanda.com/enabled: "false"
+      monitoring.redpanda.com/enabled: "true"
       team: data-platform
     name: full-featured-pool-hot-2
     namespace: full-featured
@@ -18848,7 +18902,7 @@
       app.kubernetes.io/instance: monitoring
       app.kubernetes.io/managed-by: redpanda-operator
       app.kubernetes.io/name: redpanda
-      monitoring.redpanda.com/enabled: "false"
+      monitoring.redpanda.com/enabled: "true"
     name: monitoring
     namespace: monitoring
   spec:
@@ -18882,6 +18936,34 @@
     type: ClusterIP
   status:
     loadBalancer: {}
+- apiVersion: monitoring.coreos.com/v1
+  kind: ServiceMonitor
+  metadata:
+    labels:
+      app.kubernetes.io/cluster-name: test
+      app.kubernetes.io/component: redpanda
+      app.kubernetes.io/instance: monitoring
+      app.kubernetes.io/managed-by: redpanda-operator
+      app.kubernetes.io/name: redpanda
+      release: kube-prometheus-stack
+    name: monitoring-pool-a
+    namespace: monitoring
+  spec:
+    endpoints:
+    - interval: 30s
+      path: /public_metrics
+      port: admin
+      scheme: https
+      tlsConfig:
+        ca: {}
+        cert: {}
+        insecureSkipVerify: true
+    namespaceSelector: {}
+    selector:
+      matchLabels:
+        app.kubernetes.io/instance: monitoring
+        app.kubernetes.io/name: redpanda
+        monitoring.redpanda.com/enabled: "true"
 - apiVersion: v1
   data:
     .bootstrap.json.in: '{"audit_enabled":"false","cloud_storage_cache_size":"5368709120","cloud_storage_enable_remote_read":"true","cloud_storage_enable_remote_write":"true","cloud_storage_enabled":"false","compacted_log_segment_size":"67108864","default_topic_replications":"3","enable_rack_awareness":"false","enable_sasl":"false","kafka_connection_rate_limit":"1000","kafka_enable_authorization":"false","log_segment_size_max":"268435456","log_segment_size_min":"16777216","max_compacted_log_segment_size":"536870912","storage_min_free_bytes":"1073741824"}'
@@ -19505,7 +19587,7 @@
       app.kubernetes.io/instance: monitoring
       app.kubernetes.io/managed-by: redpanda-operator
       app.kubernetes.io/name: redpanda
-      monitoring.redpanda.com/enabled: "false"
+      monitoring.redpanda.com/enabled: "true"
     name: monitoring-pool-a-0
     namespace: monitoring
   spec:
@@ -19550,7 +19632,7 @@
       app.kubernetes.io/instance: monitoring
       app.kubernetes.io/managed-by: redpanda-operator
       app.kubernetes.io/name: redpanda
-      monitoring.redpanda.com/enabled: "false"
+      monitoring.redpanda.com/enabled: "true"
     name: monitoring-pool-a-1
     namespace: monitoring
   spec:
@@ -19595,7 +19677,7 @@
       app.kubernetes.io/instance: monitoring
       app.kubernetes.io/managed-by: redpanda-operator
       app.kubernetes.io/name: redpanda
-      monitoring.redpanda.com/enabled: "false"
+      monitoring.redpanda.com/enabled: "true"
     name: monitoring-pool-a-2
     namespace: monitoring
   spec:

operator/multicluster/testdata/render-cases.txtar
@@ -576,9 +576,6 @@ kind: StretchCluster
 metadata:
   name: cluster
   namespace: default
-spec:
-  monitoring:
-    enabled: true
 ---
 apiVersion: cluster.redpanda.com/v1alpha2
 kind: RedpandaBrokerPool
@@ -591,6 +588,11 @@ spec:
     kind: StretchCluster
     name: cluster
   replicas: 3
+  monitoring:
+    enabled: true
+    scrapeInterval: 30s
+    labels:
+      release: kube-prometheus-stack
 -- storage-hostpath --
 apiVersion: cluster.redpanda.com/v1alpha2
 kind: StretchCluster
@@ -1042,8 +1044,6 @@ spec:
       port: 33145
       tls:
         cert: default
-  monitoring:
-    enabled: true
   auditLogging:
     enabled: true
   storage:
@@ -1089,6 +1089,8 @@ spec:
     kind: StretchCluster
     name: cluster
   replicas: 3
+  monitoring:
+    enabled: true
   additionalRedpandaCmdFlags:
     - "--idle-poll-time-us=0"
   additionalSelectorLabels:
@@ -1108,6 +1110,8 @@ spec:
     kind: StretchCluster
     name: cluster
   replicas: 2
+  monitoring:
+    enabled: true
   additionalSelectorLabels:
     tier: cold
 -- flat-network --